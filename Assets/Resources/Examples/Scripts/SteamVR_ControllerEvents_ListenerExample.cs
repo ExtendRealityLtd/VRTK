78c7d7885dcfacef15189db4f84c3dd54a4a607f
Theston E. Fox <thestonefox@users.noreply.github.com>
2016-04-24 22:44:29 +0100

    feat(Controller): add button event listener script
    A new Controller Events script has been added that listens for the
    interactions with the VR controller and emits an event when a button
    is pressed or released (also touched in the case of the touchpad).
    It also holds the current state of each action in a public boolean
    so it's possible to determine if a button is being continually held
    down.
    
    A new example scene has been added which demonstrates the Controller
    events along with an example script which just logs the captured events
    to the console.

---
+++ Assets/Resources/Examples/Scripts/SteamVR_ControllerEvents_ListenerExample.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteamVR_ControllerEvents_ListenerExample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (GetComponent<SteamVR_ControllerEvents>() == null)
+        {
+            Debug.LogError("SteamVR_ControllerEvents_ListenerExample is required to be attached to a SteamVR Controller that has the SteamVR_ControllerEvents script attached to it");
+            return;
+        }
+        
+        //Setup controller event listeners
+        GetComponent<SteamVR_ControllerEvents>().TriggerClicked += new ControllerClickedEventHandler(DoTriggerClicked);
+        GetComponent<SteamVR_ControllerEvents>().TriggerUnclicked += new ControllerClickedEventHandler(DoTriggerUnclicked);
+
+        GetComponent<SteamVR_ControllerEvents>().ApplicationMenuClicked += new ControllerClickedEventHandler(DoApplicationMenuClicked);
+        GetComponent<SteamVR_ControllerEvents>().ApplicationMenuUnclicked += new ControllerClickedEventHandler(DoApplicationMenuUnclicked);
+
+        GetComponent<SteamVR_ControllerEvents>().GripClicked += new ControllerClickedEventHandler(DoGripClicked);
+        GetComponent<SteamVR_ControllerEvents>().GripUnclicked += new ControllerClickedEventHandler(DoGripUnclicked);
+
+        GetComponent<SteamVR_ControllerEvents>().TouchpadClicked += new ControllerClickedEventHandler(DoTouchpadClicked);
+        GetComponent<SteamVR_ControllerEvents>().TouchpadUnclicked += new ControllerClickedEventHandler(DoTouchpadUnclicked);
+
+        GetComponent<SteamVR_ControllerEvents>().TouchpadTouched += new ControllerClickedEventHandler(DoTouchpadTouched);
+        GetComponent<SteamVR_ControllerEvents>().TouchpadUntouched += new ControllerClickedEventHandler(DoTouchpadUntouched);
+    }
+
+    void DebugLogger(uint index, string button, string action, Vector2 touchpadAxis)
+    {
+        Debug.Log("Controller on index '" + index + "' " + button + " has been " + action + " / trackpad axis at: " + touchpadAxis);
+    }
+
+    void DoTriggerClicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TRIGGER", "pressed down", e.touchpadAxis);
+    }
+
+    void DoTriggerUnclicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TRIGGER", "released", e.touchpadAxis);
+    }
+
+    void DoApplicationMenuClicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "APPLICATION MENU", "pressed down", e.touchpadAxis);
+    }
+
+    void DoApplicationMenuUnclicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "APPLICATION MENU", "released", e.touchpadAxis);
+    }
+
+    void DoGripClicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "GRIP", "pressed down", e.touchpadAxis);
+    }
+
+    void DoGripUnclicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "GRIP", "released", e.touchpadAxis);
+    }
+
+    void DoTouchpadClicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "pressed down", e.touchpadAxis);
+    }
+
+    void DoTouchpadUnclicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "released", e.touchpadAxis);
+    }
+
+    void DoTouchpadTouched(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "touched", e.touchpadAxis);
+    }
+
+    void DoTouchpadUntouched(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "untouched", e.touchpadAxis);
+    }
+}
---
+++ Assets/Resources/Scripts/SteamVR_ControllerEvents.cs
@@ -0,0 +1,166 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct ControllerClickedEventArgs
+{
+    public uint controllerIndex;
+    public Vector2 touchpadAxis;
+}
+
+public delegate void ControllerClickedEventHandler(object sender, ControllerClickedEventArgs e);
+
+public class SteamVR_ControllerEvents : MonoBehaviour {
+    public bool triggerPressed = false;
+    public bool applicationMenuPressed = false;
+    public bool touchpadPressed = false;
+    public bool touchpadTouched = false;
+    public bool gripPressed = false;
+
+    public event ControllerClickedEventHandler TriggerClicked;
+    public event ControllerClickedEventHandler TriggerUnclicked;
+
+    public event ControllerClickedEventHandler ApplicationMenuClicked;
+    public event ControllerClickedEventHandler ApplicationMenuUnclicked;
+
+    public event ControllerClickedEventHandler GripClicked;
+    public event ControllerClickedEventHandler GripUnclicked;
+
+    public event ControllerClickedEventHandler TouchpadClicked;
+    public event ControllerClickedEventHandler TouchpadUnclicked;
+
+    public event ControllerClickedEventHandler TouchpadTouched;
+    public event ControllerClickedEventHandler TouchpadUntouched;
+
+    private uint controllerIndex;
+    private SteamVR_TrackedObject trackedController;
+    private SteamVR_Controller.Device device;
+
+    void Awake()
+    {
+        trackedController = GetComponent<SteamVR_TrackedObject>();        
+    }
+
+    public virtual void OnTriggerClicked(ControllerClickedEventArgs e)
+    {
+        if (TriggerClicked != null)
+            TriggerClicked(this, e);
+    }
+
+    public virtual void OnTriggerUnclicked(ControllerClickedEventArgs e)
+    {
+        if (TriggerUnclicked != null)
+            TriggerUnclicked(this, e);
+    }
+
+    public virtual void OnApplicationMenuClicked(ControllerClickedEventArgs e)
+    {
+        if (ApplicationMenuClicked != null)
+            ApplicationMenuClicked(this, e);
+    }
+
+    public virtual void OnApplicationMenuUnclicked(ControllerClickedEventArgs e)
+    {
+        if (ApplicationMenuUnclicked != null)
+            ApplicationMenuUnclicked(this, e);
+    }
+
+    public virtual void OnGripClicked(ControllerClickedEventArgs e)
+    {
+        if (GripClicked != null)
+            GripClicked(this, e);
+    }
+
+    public virtual void OnGripUnclicked(ControllerClickedEventArgs e)
+    {
+        if (GripUnclicked != null)
+            GripUnclicked(this, e);
+    }
+
+    public virtual void OnTouchpadClicked(ControllerClickedEventArgs e)
+    {
+        if (TouchpadClicked != null)
+            TouchpadClicked(this, e);
+    }
+
+    public virtual void OnTouchpadUnclicked(ControllerClickedEventArgs e)
+    {
+        if (TouchpadUnclicked != null)
+            TouchpadUnclicked(this, e);
+    }
+
+    public virtual void OnTouchpadTouched(ControllerClickedEventArgs e)
+    {
+        if (TouchpadTouched != null)
+            TouchpadTouched(this, e);
+    }
+
+    public virtual void OnTouchpadUntouched(ControllerClickedEventArgs e)
+    {
+        if (TouchpadUntouched != null)
+            TouchpadUntouched(this, e);
+    }
+
+    ControllerClickedEventArgs SetButtonEvent(ref bool buttonBool, bool value)
+    {
+        buttonBool = value;
+        ControllerClickedEventArgs e;
+        e.controllerIndex = controllerIndex;
+        e.touchpadAxis = device.GetAxis();
+        return e;
+    }
+
+    void Update()
+    {
+        controllerIndex = (uint)trackedController.index;
+        device = SteamVR_Controller.Input((int)controllerIndex);
+
+        //Trigger
+        if(device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            OnTriggerClicked(SetButtonEvent(ref triggerPressed, true));
+        } else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            OnTriggerUnclicked(SetButtonEvent(ref triggerPressed, false));
+        }
+
+        //ApplicationMenu
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            OnApplicationMenuClicked(SetButtonEvent(ref applicationMenuPressed, true));
+        }
+        else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            OnApplicationMenuUnclicked(SetButtonEvent(ref applicationMenuPressed, false));
+        }
+
+        //Grip
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            OnGripClicked(SetButtonEvent(ref gripPressed, true));
+        }
+        else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Grip))
+        {
+            OnGripUnclicked(SetButtonEvent(ref gripPressed, false));
+        }
+
+        //Touchpad Clicked
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            OnTouchpadClicked(SetButtonEvent(ref touchpadPressed, true));
+        }
+        else if (device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            OnTouchpadUnclicked(SetButtonEvent(ref touchpadPressed, false));
+        }
+
+        //Touchpad Touched
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            OnTouchpadTouched(SetButtonEvent(ref touchpadTouched, true));
+        }
+        else if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            OnTouchpadUntouched(SetButtonEvent(ref touchpadTouched, false));
+        }
+    }
+}
