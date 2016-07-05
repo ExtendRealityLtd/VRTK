5fa29d7f159b00d9e719455f16d1d4f0bfc6b6b1
Theston E. Fox <thestonefox@users.noreply.github.com>
2016-07-05 11:27:04 +0100

    feat(UI): add ability to interact with unity ui
    Unity provides a collection of UI elements that are traditionally for
    use with a mouse or a touch screen. It is now possible to use these
    elements within VR utilising the new `VRTK_UIPointer` script that can
    be attached to any object such as the controllers or headset and it
    casts a ray out to the UI elements for interaction.
    
    The `Pointer` alias button on the associated controller determines if
    the ray is being cast or not. The `Use` alias button on the
    associated controller determines if the UI element has been clicked.
    
    It works well in conjucntion with the Simple Pointer to provide visual
    feedback to where the ray is hitting.
    
    The UIPointer works by replacing the `Standalone Input Module` on the
    `EventSystem` with a custom Input Module. It also finds all canvases
    that are set to `World Space` and replaces the default
    `Graphic Raycaster` with a custom one that can identify the rays
    coming from the in game world objects.
    
    The UIPointer script contains the following parameters:
    
      * `Controller` - The controller which will operate the pointer and
      use functions for the UI Pointer.
    
    An example scene has been added to demonstrate the feature.

---
+++ Assets/SteamVR_Unity_Toolkit/Scripts/VRTK_UIPointer.cs
@@ -0,0 +1,106 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.EventSystems;
+    using System.Collections.Generic;
+
+    public class VRTK_UIPointer : MonoBehaviour
+    {
+        public VRTK_ControllerEvents controller;
+
+        [HideInInspector]
+        public PointerEventData pointerEventData;
+
+        public static VRTK_EventSystemVRInput SetEventSystem(EventSystem eventSystem)
+        {
+            if (!eventSystem)
+            {
+                Debug.LogError("A VRTK_UIPointer requires an EventSystem");
+            }
+
+            //disable existing standalone input module
+            var standaloneInputModule = eventSystem.gameObject.GetComponent<StandaloneInputModule>();
+            if (standaloneInputModule.enabled)
+            {
+                standaloneInputModule.enabled = false;
+            }
+
+            //if it doesn't already exist, add the custom event system
+            var eventSystemInput = eventSystem.GetComponent<VRTK_EventSystemVRInput>();
+            if (!eventSystemInput)
+            {
+                eventSystemInput = eventSystem.gameObject.AddComponent<VRTK_EventSystemVRInput>();
+                eventSystemInput.Initialise();
+            }
+
+            return eventSystemInput;
+        }
+
+        public static void SetWorldCanvas(Canvas canvas)
+        {
+            if (canvas.renderMode != RenderMode.WorldSpace)
+            {
+                return;
+            }
+
+            //copy public params then disable existing graphic raycaster
+            var defaultRaycaster = canvas.gameObject.GetComponent<GraphicRaycaster>();
+            var customRaycaster = canvas.gameObject.GetComponent<VRTK_UIGraphicRaycaster>();
+            //if it doesn't already exist, add the custom raycaster
+            if (!customRaycaster)
+            {
+                customRaycaster = canvas.gameObject.AddComponent<VRTK_UIGraphicRaycaster>();
+            }
+
+            if (defaultRaycaster && defaultRaycaster.enabled)
+            {
+                customRaycaster.ignoreReversedGraphics = defaultRaycaster.ignoreReversedGraphics;
+                customRaycaster.blockingObjects = defaultRaycaster.blockingObjects;
+                defaultRaycaster.enabled = false;
+            }
+
+            //add a box collider and background image to ensure the rays always hit
+            var canvasSize = canvas.GetComponent<RectTransform>().sizeDelta;
+            if (!canvas.gameObject.GetComponent<BoxCollider>())
+            {
+                var canvasBoxCollider = canvas.gameObject.AddComponent<BoxCollider>();
+                canvasBoxCollider.size = new Vector3(canvasSize.x, canvasSize.y, 10f);
+                canvasBoxCollider.center = new Vector3(0f, 0f, 5f);
+            }
+
+            if (!canvas.gameObject.GetComponent<Image>())
+            {
+                canvas.gameObject.AddComponent<Image>().color = Color.clear;
+            }
+        }
+
+        private void Start()
+        {
+            ConfigureEventSystem();
+            ConfigureWorldCanvases();
+            if (controller == null)
+            {
+                controller = this.GetComponent<VRTK_ControllerEvents>();
+            }
+        }
+
+        private void ConfigureEventSystem()
+        {
+            var eventSystem = GameObject.FindObjectOfType<EventSystem>();
+            var eventSystemInput = SetEventSystem(eventSystem);
+
+            pointerEventData = new PointerEventData(eventSystem);
+            pointerEventData.pointerId = (int)this.GetComponent<SteamVR_TrackedObject>().index + 1000;
+            eventSystemInput.pointers.Add(this);
+        }
+
+        private void ConfigureWorldCanvases()
+        {
+            foreach (var canvas in GameObject.FindObjectsOfType<Canvas>())
+            {
+                SetWorldCanvas(canvas);
+            }
+        }
+    }
+}
