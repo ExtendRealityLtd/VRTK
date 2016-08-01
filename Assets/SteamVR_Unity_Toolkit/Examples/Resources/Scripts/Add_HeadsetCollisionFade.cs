bda21c1b5671aed07240fd4577305d8a589fe960
Theston E. Fox <thestonefox@users.noreply.github.com>
2016-08-01 22:18:29 +0100

    chore(Examples): set example scenes up correctly per unity version
    As some of the features in SteamVR changed between Unity 5.3 and
    Unity 5.4 it means that the example scenes that use the
    Headset Collision Fade script didn't work in Unity 5.4 as they were
    set up for the SteamVR usage in Unity 5.3.
    
    This commit adds a helper script that adds the Headset Collision Fade
    script to the correct game object dependant on the version at
    edit time. This is better than doing it at runtime as then users can
    view the game objects in the editor without playing the scene and
    see where the scripts are supposed to go.
    
    There are also setup scripts for example scene 031 and 034 that set
    up the relevant scripts on the Camera on launch of editor.

---
+++ Assets/SteamVR_Unity_Toolkit/Examples/Resources/Scripts/Add_HeadsetCollisionFade.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using VRTK;
+
+[ExecuteInEditMode]
+public class Add_HeadsetCollisionFade : MonoBehaviour
+{
+    private bool initalised = false;
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (!initalised)
+        {
+            var headset = VRTK_DeviceFinder.HeadsetTransform();
+            if (!headset.GetComponent<VRTK_HeadsetCollisionFade>())
+            {
+                headset.gameObject.AddComponent<VRTK_HeadsetCollisionFade>();
+            }
+            initalised = true;
+        }
+    }
+#endif
+}
---
+++ Assets/SteamVR_Unity_Toolkit/Examples/Resources/Scripts/Setup_Scene_034.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using VRTK;
+
+[ExecuteInEditMode]
+public class Setup_Scene_034 : MonoBehaviour
+{
+    private bool initalised = false;
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (!initalised)
+        {
+            var headset = VRTK_DeviceFinder.HeadsetTransform();
+            var controllerManager = FindObjectOfType<SteamVR_ControllerManager>();
+            var controllerEvents = controllerManager.left.GetComponent<VRTK_ControllerEvents>();
+
+            if (!headset.GetComponent<VRTK_SimplePointer>())
+            {
+                var pointer = headset.gameObject.AddComponent<VRTK_SimplePointer>();
+
+                pointer.controller = controllerEvents;
+                pointer.enableTeleport = false;
+                pointer.pointerVisibility = VRTK_WorldPointer.pointerVisibilityStates.Always_Off;
+                pointer.pointerLength = 100f;
+                pointer.showPointerTip = true;
+            }
+
+            if (!headset.GetComponent<VRTK_UIPointer>())
+            {
+                var uiPointer = headset.gameObject.AddComponent<VRTK_UIPointer>();
+                uiPointer.controller = controllerEvents;
+                uiPointer.ignoreCanvasWithTagOrClass = "ExcludeTeleport";
+            }
+            initalised = true;
+        }
+    }
+#endif
+}
