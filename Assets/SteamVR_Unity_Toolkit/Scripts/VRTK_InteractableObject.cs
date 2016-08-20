bec033fbdd9a69b81b3efdd055a0ee7bbeeccb5f
Jonathan Schenker <jonathansmac55@gmail.com>
2016-08-20 12:42:29 -0500

    feat(Interaction): add interactable object custom grab and use alias

--- Assets/SteamVR_Unity_Toolkit/Scripts/VRTK_InteractableObject.cs
+++ Assets/SteamVR_Unity_Toolkit/Scripts/VRTK_InteractableObject.cs
@@ -55,6 +55,7 @@ namespace VRTK
         public ControllerHideMode hideControllerOnTouch = ControllerHideMode.Default;
 
         [Header("Grab Interactions", order = 2)]
+        public VRTK_ControllerEvents.ButtonAlias GrabButton = VRTK_ControllerEvents.ButtonAlias.Undefined;
         public bool isGrabbable = false;
         public bool isDroppable = true;
         public bool isSwappable = true;
@@ -75,6 +76,7 @@ namespace VRTK
         public float onGrabCollisionDelay = 0f;
 
         [Header("Use Interactions", order = 4)]
+        public VRTK_ControllerEvents.ButtonAlias UseButton = VRTK_ControllerEvents.ButtonAlias.Undefined;
         public bool isUsable = false;
         public bool useOnlyIfGrabbed = false;
         public bool holdButtonToUse = true;
