20bab8a365bf5621d3a377e1585ce2a0037ced37
sidcitris <sidcitris@gmail.com>
2016-09-04 00:21:23 -0400

    feat(Climbing): Added Climbing Stamina
    Add climbing stamina class that extends the existing VRTK_PlayerClimb
    class. The VRTK_PlayerClimbStamina script is added to the CameraRig in
    of place the previous climbing script.
    
    The players min reach and max reach can be set where the distance in
    the xz plane between the controller and the head determine the effect
    on stamina. A distance less than min reach will not decrease stamina
    while a distance beyone max reach will apply the maximum damge to
    stamina.
    
    The player can regenerate stamina when either no hands are holding a
    climbable object, or when both hands are holding climbable objects.
    The VRTK currently doesn't support grabbing one object with both hands
    but should work when it is implemented.
    
    Example 039_CameraRig_ClimbingStamina has been added to demonstrate
    the climbing stamina as well as a useful grip stamina visualization
    prefab which has been added.

---
+++ Assets/VRTK/Examples/Resources/Scripts/GripStamina.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using VRTK;
+
+public class GripStamina : MonoBehaviour {
+
+    public enum Orientation
+    {
+        ClockWise,
+        CounterClockWise    
+    }
+
+    public VRTK_PlayerClimbStamina staminaComponent;
+    public Sprite defaultImage;    
+    public int numIcons = 5;
+    public bool rotateIcons = true;
+    public bool isShown = true;
+    public Orientation orientation = Orientation.CounterClockWise;
+
+    private List<GameObject> gripIcons = new List<GameObject>();
+    private Color iconColor = Color.green;
+    private float normalizedCurrentStamina;
+    
+    private void Awake()
+    {
+        if (Application.isPlaying)
+        {
+            if (!isShown)
+            {
+                transform.localScale = Vector3.zero;
+            }
+            GenerateIcons();
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        normalizedCurrentStamina = Mathf.Clamp01(staminaComponent.CurrentStamina / staminaComponent.maxStamina);
+        if (normalizedCurrentStamina < 1f)
+        {
+            transform.localScale = Vector3.one;
+            RefreshIcons();
+        } else
+        {
+            transform.localScale = Vector3.zero;
+        }
+    }
+
+    private void RefreshIcons()
+    {
+        // Update Icon Color
+        iconColor.r = 2 * (1 - normalizedCurrentStamina);
+        iconColor.g = 2 * (normalizedCurrentStamina);
+        iconColor.b = 0f;
+
+        // Update Icons
+        for (int i = 0; i < gripIcons.Count; i++)
+        {
+            GameObject icon = gripIcons[i];
+            icon.GetComponent<Image>().color = iconColor;
+
+            //Integer division caused issues here, converting to float fixed it
+            float index = (float)i;
+            if (normalizedCurrentStamina >= (index + 1f) / numIcons)
+            {
+                icon.transform.localScale = Vector3.one / (numIcons * 1.5f);
+            }
+            else
+            {                
+                float percentScale = Mathf.Clamp01((normalizedCurrentStamina - index / numIcons) / ((index + 1f) / numIcons - index / numIcons));
+                icon.transform.localScale = Vector3.one / (numIcons * 1.5f) * percentScale;
+            }
+        }
+    }
+
+    //Creates all the button Arcs and populates them with desired icons
+    private void GenerateIcons()
+    {        
+        for (int i = 0; i < numIcons; i++)
+        {
+            // Initial placement/instantiation
+            GameObject newButton = new GameObject();
+            newButton.layer = 4;
+
+            Image image = newButton.AddComponent<Image>();
+            image.sprite= defaultImage;
+            image.color = iconColor;
+
+            newButton.GetComponent<RectTransform>().offsetMax = Vector2.zero;
+            newButton.GetComponent<RectTransform>().offsetMin = Vector2.zero;
+
+            newButton.transform.SetParent(transform);
+            newButton.transform.localScale = Vector3.one / (numIcons * 1.5f);
+            
+            float angle = ((180 / numIcons) * i);
+            newButton.transform.localEulerAngles = new Vector3(0, 0, angle);
+
+            if (orientation == Orientation.ClockWise)
+            {
+                angle *= -1;                 
+            }
+
+            float angleRad = (angle * Mathf.PI) / 180f;
+            Vector2 angleVector = new Vector2(-Mathf.Cos(angleRad), -Mathf.Sin(angleRad));
+            newButton.transform.localPosition = Vector3.zero + (Vector3)angleVector * 100f;            
+
+            newButton.GetComponent<RectTransform>().anchorMin = new Vector2(0f, 0f);
+            newButton.GetComponent<RectTransform>().anchorMax = new Vector2(1f, 1f);
+            
+            //Rotate icons all vertically if desired
+            if (!rotateIcons)
+            {
+                newButton.transform.eulerAngles = GetComponentInParent<Canvas>().transform.eulerAngles;
+            }            
+            
+            gripIcons.Add(newButton);
+        }
+    }    
+}
---
+++ Assets/VRTK/Scripts/VRTK_PlayerClimbStamina.cs
@@ -0,0 +1,228 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public class VRTK_PlayerClimbStamina : VRTK_PlayerClimb
+    {
+        public enum ClimbingControllers
+        {
+            Both,
+            Left_Only,
+            Right_Only,
+            None
+        }
+
+        public float minReach = 0.05f;
+        public float maxReach = 0.8f;
+        public float maxStamina = 100f;
+        public float staminaRecoveryRate = 20f;
+        public float staminaDrainRate = 40f; 
+        
+        private ClimbingControllers climbingControllers = ClimbingControllers.None;
+        private GameObject climbingObjectL;
+        private GameObject climbingObjectR;
+        private GameObject secondaryHold;
+        private Vector3 secondaryHoldStartingPosition;
+        private float detachThreshold = 100f;
+        private float climbingObjectDistance;
+        private float climbingObjectDifficulty = 1.0f;
+        private float currentStamina;
+
+        // Accessor for current stamina. Want to be able to set it in case of stamina regen items or penalties
+        public float CurrentStamina
+        {
+            get
+            {
+                return currentStamina;
+            }
+
+            set
+            {
+                currentStamina = value;
+            }
+        }
+
+        // Call base class awake and set current stamina to the max value 
+        protected override void Awake()
+        {
+            base.Awake();
+            CurrentStamina = maxStamina;
+        }
+
+        // Call base class OnEnable and intialize grab listeners
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            InitGrabEventListeners(VRTK_SDK_Bridge.GetControllerLeftHand(), true);
+            InitGrabEventListeners(VRTK_SDK_Bridge.GetControllerRightHand(), true);
+        }
+
+        // Call base class OnDisable, clean up, and remove grab event listeners
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            climbingObjectL = null;
+            climbingObjectR = null;
+            climbingControllers = ClimbingControllers.None;
+            InitGrabEventListeners(VRTK_SDK_Bridge.GetControllerLeftHand(), false);
+            InitGrabEventListeners(VRTK_SDK_Bridge.GetControllerRightHand(), false);
+        }
+
+        // Initialize Grab Event listeners
+        private void InitGrabEventListeners(GameObject controller, bool state)
+        {
+            if (controller)
+            {
+                var grabbingController = controller.GetComponent<VRTK_InteractGrab>();
+                if (grabbingController)
+                {
+                    if (state)
+                    {
+                        grabbingController.ControllerGrabInteractableObject += new ObjectInteractEventHandler(OnGrabObject);
+                        grabbingController.ControllerUngrabInteractableObject += new ObjectInteractEventHandler(OnUngrabObject);
+                    }
+                    else
+                    {
+                        grabbingController.ControllerGrabInteractableObject -= new ObjectInteractEventHandler(OnGrabObject);
+                        grabbingController.ControllerUngrabInteractableObject -= new ObjectInteractEventHandler(OnUngrabObject);
+                    }
+                }
+            }
+        }
+
+        // When an object is grabbed, check which hand, and set previously grabbed object to secondary hold if one is still being held
+        private void OnGrabObject(object sender, ObjectInteractEventArgs e)
+        {
+            if (e.controllerIndex == VRTK_DeviceFinder.GetControllerIndex(VRTK_SDK_Bridge.GetControllerLeftHand()))
+            {
+                climbingObjectL = e.target;
+                if (climbingControllers == ClimbingControllers.Right_Only)
+                {
+                    secondaryHold = climbingObjectR;
+                    secondaryHoldStartingPosition = secondaryHold.GetComponent<VRTK_InteractableObject>().GetGrabbingObject().transform.position;
+                    climbingControllers = ClimbingControllers.Both;                    
+                }
+                else
+                {
+                    secondaryHold = null;
+                    climbingControllers = ClimbingControllers.Left_Only;
+                }
+            }
+            else if (e.controllerIndex == VRTK_DeviceFinder.GetControllerIndex(VRTK_SDK_Bridge.GetControllerRightHand()))
+            {
+                climbingObjectR = e.target;
+                if (climbingControllers == ClimbingControllers.Left_Only)
+                {
+                    secondaryHold = climbingObjectL;
+                    secondaryHoldStartingPosition = secondaryHold.GetComponent<VRTK_InteractableObject>().GetGrabbingObject().transform.position;
+                    climbingControllers = ClimbingControllers.Both;                    
+                }
+                else
+                {
+                    secondaryHold = null;                    
+                    climbingControllers = ClimbingControllers.Right_Only;
+                }
+            }
+        }
+
+        // Clear climbing object and secondary hold. Set ClimbingControllers to approriate value
+        private void OnUngrabObject(object sender, ObjectInteractEventArgs e)
+        {
+            secondaryHold = null;
+            if (e.controllerIndex == VRTK_DeviceFinder.GetControllerIndex(VRTK_SDK_Bridge.GetControllerLeftHand()))
+            {                
+                climbingObjectL = null;
+                if (climbingControllers == ClimbingControllers.Both)
+                {
+                    climbingControllers = ClimbingControllers.Right_Only;
+                    secondaryHold = null;
+                }
+                else
+                {
+                    climbingControllers = ClimbingControllers.None;
+                }
+            }
+            else if (e.controllerIndex == VRTK_DeviceFinder.GetControllerIndex(VRTK_SDK_Bridge.GetControllerRightHand()))
+            {
+                climbingObjectR = null;
+                if (climbingControllers == ClimbingControllers.Both)
+                {
+                    climbingControllers = ClimbingControllers.Left_Only;
+                    secondaryHold = null;
+                }
+                else
+                {
+                    climbingControllers = ClimbingControllers.None;
+                }
+            }
+        }
+
+        // Update base class, then make updates based on current climbing hands 
+        protected override void Update()
+        {
+            base.Update();
+
+            if (climbingControllers == ClimbingControllers.Both) 
+            {
+                // Regen Stamina     
+                UpdateBothHands();
+            }
+            else if (climbingControllers == ClimbingControllers.Left_Only) 
+            {
+                // Remove Stamina
+                UpdateSingleHand(climbingObjectL);               
+            }
+            else if (climbingControllers == ClimbingControllers.Right_Only)
+            {
+                // Remove Stamina
+                UpdateSingleHand(climbingObjectR);
+            }
+            else if (climbingControllers == ClimbingControllers.None) 
+            {
+                // Regen Stamina
+                UpdateNoHands();
+            }
+        }
+
+        // Update both hands
+        protected void UpdateBothHands()
+        {
+            if (secondaryHold)
+            {
+                float distance = Vector3.Distance(secondaryHold.GetComponent<VRTK_InteractableObject>().GetGrabbingObject().transform.position, secondaryHoldStartingPosition);
+                if (distance > (detachThreshold / 1000))
+                {
+                    secondaryHold.GetComponent<VRTK_InteractableObject>().ForceStopInteracting();
+                    secondaryHold = null;
+                }
+            }
+            CurrentStamina = Mathf.Min(CurrentStamina + (Time.deltaTime * staminaRecoveryRate), maxStamina);
+        }
+
+        // Update single hand
+        protected void UpdateSingleHand(GameObject grabbedObject)
+        {
+            climbingObjectDistance = GetNormalizedClimbDistance(grabbedObject.GetComponent<VRTK_InteractableObject>().GetGrabbingObject());
+            CurrentStamina = Mathf.Max(CurrentStamina - (Time.deltaTime * staminaDrainRate * climbingObjectDistance * climbingObjectDistance * climbingObjectDifficulty), 0.0f);
+            if (CurrentStamina == 0f)
+            {               
+                grabbedObject.GetComponent<VRTK_InteractableObject>().ForceStopInteracting();
+            }
+        }
+
+        // Update when nothhing is currently being grabbed
+        protected void UpdateNoHands()
+        {
+            CurrentStamina = Mathf.Min(CurrentStamina + 2 * (Time.deltaTime * staminaRecoveryRate), maxStamina);
+        }
+
+        // Get a normalized climb value between 0 and 1 based on reach distances and current hand distance from head.
+        protected float GetNormalizedClimbDistance(GameObject climbingObject)
+        {
+            float normalizedDistance = Mathf.Pow(climbingObject.transform.position.x - headCamera.position.x, 2) + Mathf.Pow(climbingObject.transform.position.z - headCamera.position.z, 2);
+            normalizedDistance = Mathf.Clamp01((normalizedDistance - minReach * minReach) / (maxReach * maxReach - minReach * minReach));
+            return normalizedDistance;
+        }
+    }
+}
