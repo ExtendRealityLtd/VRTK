5fef690da9de49fd55d138f22e5d317055ce5375
Theston E. Fox <thestonefox@users.noreply.github.com>
2016-10-17 13:15:32 +0100

    feat(Interaction): add fire extinguisher interactable object example
    A new Fire Extinguisher interactable object has been added to the
    `021_Controller_GrabbingObjectsWithJoints` scene to show how two
    interactable objects connected with a joint can work together.
    
    The fire extinguisher also shows how the pressure of the trigger press
    can be used to animate the lever.

---
+++ Assets/VRTK/Examples/Resources/Scripts/FireExtinguisher_Base.cs
@@ -0,0 +1,56 @@
+﻿namespace VRTK.Examples
+{
+    using System;
+    using UnityEngine;
+
+    public class FireExtinguisher_Base : VRTK_InteractableObject
+    {
+        public Animation leverAnimation;
+        public FireExtinguisher_Sprayer sprayer;
+
+        private VRTK_ControllerEvents controllerEvents;
+        private VRTK_ControllerActions controllerActions;
+
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+            controllerEvents = usingObject.GetComponent<VRTK_ControllerEvents>();
+            controllerActions = usingObject.GetComponent<VRTK_ControllerActions>();
+        }
+
+        public override void StopUsing(GameObject previousUsingObject)
+        {
+            base.StopUsing(previousUsingObject);
+            controllerEvents = null;
+            controllerActions = null;
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (controllerEvents)
+            {
+                var power = controllerEvents.GetTriggerAxis();
+                Spray(power);
+                controllerActions.TriggerHapticPulse(Convert.ToUInt16(1000f * power), 0.1f, 0.01f);
+            }
+            else
+            {
+                Spray(0f);
+            }
+        }
+
+        private void Spray(float power)
+        {
+            SetHandleFrame(power);
+            sprayer.Spray(power);
+        }
+
+        private void SetHandleFrame(float frame)
+        {
+            leverAnimation["FireExtinguisherLever"].speed = 0;
+            leverAnimation["FireExtinguisherLever"].time = frame;
+            leverAnimation.Play("FireExtinguisherLever");
+        }
+    }
+}
---
+++ Assets/VRTK/Examples/Resources/Scripts/FireExtinguisher_Sprayer.cs
@@ -0,0 +1,48 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class FireExtinguisher_Sprayer : VRTK_InteractableObject
+    {
+        public FireExtinguisher_Base baseCan;
+        public float breakDistance = 0.12f;
+        public float maxSprayPower = 5f;
+
+        private GameObject waterSpray;
+        private ParticleSystem particles;
+
+        public void Spray(float power)
+        {
+            if (power <= 0)
+            {
+                particles.Stop();
+            }
+
+            if (power > 0)
+            {
+                if (particles.isPaused || particles.isStopped)
+                {
+                    particles.Play();
+                }
+                particles.startSpeed = maxSprayPower * power;
+            }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+            waterSpray = transform.FindChild("WaterSpray").gameObject;
+            particles = waterSpray.GetComponent<ParticleSystem>();
+            particles.Stop();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (Vector3.Distance(transform.position, baseCan.transform.position) > breakDistance)
+            {
+                ForceStopInteracting();
+            }
+        }
+    }
+}
