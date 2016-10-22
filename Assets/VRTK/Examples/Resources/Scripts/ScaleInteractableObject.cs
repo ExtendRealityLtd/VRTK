bbd71d11103be0e1e5bb1e0f31724984c8a47b39
Serban Porumbescu <serban@hiddenelephant.com>
2016-10-21 17:48:58 -0700

    feat(Scaling): add an example to show how to scale an object using one or two hands.
    This is in support of issue thestonefox/VRTK#31
    
    Extend VRTK_InteractableObject to demonstrate an approach to uniform object scaling. Objects in the demo scene are configured with "Precision Grab" either on or off. Scale happens at the objects center and can appear to pull away from the controller when using Precision Grab and you are shrinking the object.

---
+++ Assets/VRTK/Examples/Resources/Scripts/ScaleInteractableObject.cs
@@ -0,0 +1,71 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+
+    public class ScaleInteractableObject : VRTK_InteractableObject
+    {
+        private const float MinScale = 0.01f;
+
+        private float initialDistance = 0f;
+        private float initialScale = 1f;
+
+        private bool scaleInProgress = false;
+        private bool kinematicState = false;
+
+        private GameObject controllerUsing = null;
+
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+            controllerUsing = usingObject;
+
+            initialDistance = VRTK.Utilities.GetDistanceBetweenControllers(); 
+            initialScale = transform.localScale.x;
+            scaleInProgress = true;
+
+            // remember kinematic state so we can reset it when we're done
+            var rBody = GetComponent<Rigidbody>();
+            kinematicState = rBody.isKinematic;
+            rBody.isKinematic = true;
+        }
+
+        public override void StopUsing(GameObject previousUsingObject)
+        {
+            base.StopUsing(previousUsingObject);
+            controllerUsing = null;
+            scaleInProgress = false;
+            var rBody = GetComponent<Rigidbody>();
+            rBody.isKinematic = kinematicState;
+        }
+
+        protected override void Update()
+        {
+            if (!scaleInProgress)
+            {
+                return;
+            }
+
+            float currentDistance = VRTK.Utilities.GetDistanceBetweenControllers();
+            float newScale = (initialScale / initialDistance) * currentDistance;
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+
+            // make sure object's scale doesn't go negative, colliders don't like that
+            if (transform.localScale.x < MinScale)
+            {
+                transform.localScale = new Vector3(MinScale, MinScale, MinScale);
+            }
+
+            // force continued touching of the object, makes scaling easier as object gets tiny or huge 
+            if (controllerUsing != null)
+            {
+                controllerUsing.GetComponent<VRTK_InteractTouch>().ForceTouch(gameObject);
+            }
+            else
+            {
+                Debug.Log("controllerUsing == null");
+            }
+        }
+
+    }
+
+}
