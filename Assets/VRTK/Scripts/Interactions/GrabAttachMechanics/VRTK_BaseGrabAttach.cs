f98c098c48f322f764d960a70a8081b76465e4a4
Theston E. Fox <thestonefox@users.noreply.github.com>
2016-11-14 10:52:30 +0000

    feat(Interaction): add option to apply point distance on throw
    Previously, the object velocity on throw was always being upadted to
    include the distance of the object from the attach point on the
    controller. Whilst this is suitable in some circumstances (e.g. a long
    stick with a ball on the end) it is not appropriate in all cases so
    it has now been made an option to allow it to be set accordingly.

--- Assets/VRTK/Scripts/Interactions/GrabAttachMechanics/VRTK_BaseGrabAttach.cs
+++ Assets/VRTK/Scripts/Interactions/GrabAttachMechanics/VRTK_BaseGrabAttach.cs
@@ -19,6 +19,8 @@ namespace VRTK.GrabAttachMechanics
         public Transform rightSnapHandle;
         [Tooltip("A Transform provided as an empty game object which must be the child of the item being grabbed and serves as an orientation point to rotate and position the grabbed item in relation to the left handed controller. If no Left Snap Handle is provided but a Right Snap Handle is provided, then the Right Snap Handle will be used in place. If no Snap Handle is provided then the object will be grabbed at its central point. Not required for `Precision Snap`.")]
         public Transform leftSnapHandle;
+        [Tooltip("If checked then when the object is thrown, the distance between the object's attach point and the controller's attach point will be used to calculate a faster throwing velocity.")]
+        public bool throwVelocityWithAttachDistance = false;
         [Tooltip("An amount to multiply the velocity of the given object when it is thrown. This can also be used in conjunction with the Interact Grab Throw Multiplier to have certain objects be thrown even further than normal (or thrown a shorter distance if a number below 1 is entered).")]
         public float throwMultiplier = 1f;
         [Tooltip("The amount of time to delay collisions affecting the object when it is first grabbed. This is useful if a game object may get stuck inside another object when it is being grabbed.")]
@@ -220,7 +222,10 @@ namespace VRTK.GrabAttachMechanics
                         objectRigidbody.angularVelocity = angularVelocity;
                     }
 
-                    objectRigidbody.velocity = objectRigidbody.GetPointVelocity(objectRigidbody.position + (objectRigidbody.position - transform.position));
+                    if (throwVelocityWithAttachDistance)
+                    {
+                        objectRigidbody.velocity = objectRigidbody.GetPointVelocity(objectRigidbody.position + (objectRigidbody.position - transform.position));
+                    }
                 }
             }
         }
