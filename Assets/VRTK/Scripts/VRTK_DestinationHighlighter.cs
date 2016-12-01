7f6dc72b6888caa7443f93b4e4aad69a7d37e262
Demo <tellutwurp@gmail.com>
2016-12-01 16:43:04 -0700

    feat(Controller Interaction): TouchpadRelease restores material
    Object remained Highlighted when releasing
    Touchpad. TouchpadReleased now restores object
    to original material

--- Assets/VRTK/Scripts/VRTK_DestinationHighlighter.cs
+++ Assets/VRTK/Scripts/VRTK_DestinationHighlighter.cs
@@ -4,22 +4,42 @@ namespace VRTK
 {
     using UnityEngine;
 
+    [RequireComponent(typeof(VRTK_SimplePointer), typeof(VRTK_ControllerEvents))]
     public class VRTK_DestinationHighlighter : MonoBehaviour
     {
         private Material originalMaterial;
         private Renderer lastHighlightedObject;
 
+        private VRTK_ControllerEvents events;
+        private VRTK_SimplePointer pointer;
+
         private void Start()
         {
-            if (GetComponent<VRTK_SimplePointer>() == null)
+            pointer = GetComponent<VRTK_SimplePointer>();
+            if (pointer == null)
             {
-                Debug.LogError("VRTK_DestinationHighLighter is required to be attached to a Controller that has the VRTK_SimplePointer script attached to it");
+                Debug.LogError("VRTK_DestinationHighlighter is required to be attached to a Controller that has the VRTK_SimplePointer script attached to it");
                 return;
             }
 
-            GetComponent<VRTK_SimplePointer>().DestinationMarkerEnter += new DestinationMarkerEventHandler(OnDestinationMarkerEnter);
-            GetComponent<VRTK_SimplePointer>().DestinationMarkerExit += new DestinationMarkerEventHandler(OnDestinationMarkerExit);
+            events = GetComponent<VRTK_ControllerEvents>();
+            if (events == null)
+            {
+                Debug.LogError("VRTK_DestinationHighlighter is required to be attached to a Controller that has the VRTK_ControllerEvents script attached to it");
+                return;
+            }
+
+            pointer.DestinationMarkerEnter += new DestinationMarkerEventHandler(OnDestinationMarkerEnter);
+            pointer.DestinationMarkerExit += new DestinationMarkerEventHandler(OnDestinationMarkerExit);
+
+            events.TouchpadReleased += new ControllerInteractionEventHandler(OnTouchpadRelease);
         }
+
+        private void OnTouchpadRelease(object sender, ControllerInteractionEventArgs e)
+        {
+            Restore();
+        }
+
         private void OnDestinationMarkerEnter(object sender, DestinationMarkerEventArgs e)
         {
             var io = e.target.GetComponent<VRTK_InteractableObject>();
@@ -35,6 +55,11 @@ namespace VRTK
         }
 
         private void OnDestinationMarkerExit(object sender, DestinationMarkerEventArgs e)
+        {
+            Restore();
+        }
+
+        private void Restore()
         {
             if (lastHighlightedObject != null && originalMaterial != null)
             {
