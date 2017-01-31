ad9f6b97bfc7db61e58ff189886e8cf6ff5eeeb1
Theston E. Fox <thestonefox@users.noreply.github.com>
2017-01-31 23:40:56 +0000

    fix(Highlighters): ensure highlighter is turned off on disable
    When the interactable object is disabled, the highlighter should also
    be turned off otherwise when the object is re-enabled it still has
    the highlighted colour even if it's not being touched.

--- Assets/VRTK/Scripts/Interactions/Highlighters/VRTK_BaseHighlighter.cs
+++ Assets/VRTK/Scripts/Interactions/Highlighters/VRTK_BaseHighlighter.cs
@@ -87,5 +87,10 @@ namespace VRTK.Highlighters
 
             return objectHighlighter;
         }
+
+        protected virtual void OnDisable()
+        {
+            Unhighlight();
+        }
     }
 }
