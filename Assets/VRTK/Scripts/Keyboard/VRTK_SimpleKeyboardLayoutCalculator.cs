7ded33ef383cf263f3bc74959dd063fa6b32c327
Daniel Friesen <d@danf.ca>
2017-02-03 02:14:43 -0800

    feat(Keyboard): Implement spacing for simple keyboard layout calculator

--- Assets/VRTK/Scripts/Keyboard/VRTK_SimpleKeyboardLayoutCalculator.cs
+++ Assets/VRTK/Scripts/Keyboard/VRTK_SimpleKeyboardLayoutCalculator.cs
@@ -22,46 +22,77 @@ namespace VRTK
         public float rowSpacing = 0f;
         [Tooltip("Side of the horizontal space in between keys")]
         public float keySpacing = 0f;
-        
+
         protected override void CalculateKeyset(BKeyset keyset, Vector2 containerSize)
         {
             BKeyArea leftArea = keyset.KeyArea("Left");
             BKeyArea rightArea = keyset.KeyArea("Right");
 
+            float areaWidth = containerSize.x / 2f - areaSpacing / 2f;
             leftArea.rKeyArea.rect = new Rect(
                 new Vector2(0, 0),
-                new Vector2(containerSize.x / 2f, containerSize.y)
+                new Vector2(areaWidth, containerSize.y)
             );
             rightArea.rKeyArea.rect = new Rect(
-                new Vector2(containerSize.x / 2f, 0),
-                new Vector2(containerSize.x / 2f, containerSize.y)
+                new Vector2(areaWidth + areaSpacing, 0),
+                new Vector2(areaWidth, containerSize.y)
             );
 
             // TODO: Implicit space+done row
             BRow[] rows = keyset.Rows();
+            float perRowSpacing = (rowSpacing * (float)(rows.Length - 1)) / (float)rows.Length;
+            float rowHeight = containerSize.y / (float)rows.Length - perRowSpacing;
+            float y = 0;
             foreach (BRow row in rows)
             {
-                CalculateAreaRowKeys(leftArea, row, row.LeftKeys(), rows.Length);
-                CalculateAreaRowKeys(rightArea, row, row.RightKeys(), rows.Length);
+                CalculateAreaRowKeys(
+                    area: leftArea,
+                    row: row,
+                    keys: row.LeftKeys(),
+                    y: y,
+                    keyHeight: rowHeight
+                );
+                CalculateAreaRowKeys(
+                    area: rightArea,
+                    row: row,
+                    keys: row.RightKeys(),
+                    y: y,
+                    keyHeight: rowHeight
+                );
+
+                y += rowHeight + rowSpacing;
             }
         }
 
-        protected virtual void CalculateAreaRowKeys(BKeyArea area, BRow row, BKey[] keys, int rowCount)
+        protected virtual void CalculateAreaRowKeys(BKeyArea area, BRow row, BKey[] keys, float y, float keyHeight)
         {
-            float keyHeight = area.rKeyArea.rect.size.y / (int)rowCount;
-            float keyWidth = area.rKeyArea.rect.size.x / (float)keys.Length;
-
+            // FUTURE: Add a BRrow.rowWeight getter that does this
+            float rowWeight = 0;
             foreach (BKey key in keys)
             {
+                rowWeight += Mathf.Max(key.key.weight, 1);
+            }
+
+            float perKeySpacing = (keySpacing * (float)(rowWeight - 1)) / (float)rowWeight;
+            float keyWidth = (area.rKeyArea.rect.size.x / (float)rowWeight) - perKeySpacing;
+
+            float x = 0;
+            foreach (BKey key in keys)
+            {
+                float keyWeight = (float)Mathf.Max(key.key.weight, 1);
+                float width = keyWidth * keyWeight + keySpacing * (keyWeight - 1f);
+
                 RKey rKey = key.Create();
                 rKey.rect = new Rect(
-                    keyWidth * (float)key.localIndex,
-                    keyHeight * (float)row.index,
-                    keyWidth,
+                    x,
+                    y,
+                    width,
                     keyHeight
                 );
 
                 area.AddKey(rKey);
+
+                x += keyWidth + keySpacing;
             }
         }
     }
