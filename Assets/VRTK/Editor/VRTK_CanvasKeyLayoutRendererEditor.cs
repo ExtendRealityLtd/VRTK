7d8ee7cb6d0682396938ca729b28912b1db3ab08
Daniel Friesen <d@danf.ca>
2017-02-04 03:06:34 -0800

    feat(Keyboard): Add issue hints to renderer and calculator inspectors

---
+++ Assets/VRTK/Editor/VRTK_CanvasKeyLayoutRendererEditor.cs
@@ -0,0 +1,24 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+    using UnityEditor;
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    [CustomEditor(typeof(VRTK_CanvasKeyLayoutRenderer), true)]
+    public class VRTK_CanvasKeyLayoutRendererEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            VRTK_CanvasKeyLayoutRenderer renderer = (VRTK_CanvasKeyLayoutRenderer)target;
+
+            if (renderer.keyTemplate == null)
+            {
+                EditorGUILayout.HelpBox("A Key Template is required", MessageType.Error);
+            }
+
+            DrawDefaultInspector();
+        }
+    }
+}
---
+++ Assets/VRTK/Editor/VRTK_KeyboardLayoutCalculatorEditor.cs
@@ -0,0 +1,24 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+    using UnityEditor;
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    [CustomEditor(typeof(VRTK_BaseKeyboardLayoutCalculator), true)]
+    public class VRTK_KeyboardLayoutCalculatorEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            VRTK_BaseKeyboardLayoutCalculator calculator = (VRTK_BaseKeyboardLayoutCalculator)target;
+
+            if ( calculator.keyboardLayout == null )
+            {
+                EditorGUILayout.HelpBox("A Keyboard Layout is required", MessageType.Error);
+            }
+
+            DrawDefaultInspector();
+        }
+    }
+}
