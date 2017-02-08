3dfc89dec8e23d3b6d5cd45ba51d9cb5810dfbcb
Thomas Ascioglu <thomas.ascioglu@gmx.de>
2017-02-08 15:20:23 +0100

    IA: Support for Leap Motion Controllers
    - provided by thomsan
    
    (cherry picked from commit ce3630a84ca0ad92bfcd2408b4b546f202d3317c)

---
+++ Assets/VRTK/SDK/LeapMotion/SDK_LeapMotionDefines.cs
@@ -0,0 +1,22 @@
+﻿// OculusVR Defines|SDK_LeapMotion|001
+namespace VRTK
+{
+    /// <summary>
+    /// Handles all the scripting define symbols for the LeapMotion
+    /// </summary>
+    public static class SDK_LeapMotionDefines
+    {
+        /// <summary>
+        /// The scripting define symbol for the OculusVR SDK.
+        /// </summary>
+        public const string ScriptingDefineSymbol = SDK_ScriptingDefineSymbolPredicateAttribute.RemovableSymbolPrefix + "SDK_LEAPMOTION";
+
+        private const string BuildTargetGroupName = "Standalone";
+
+        [SDK_ScriptingDefineSymbolPredicate(ScriptingDefineSymbol, BuildTargetGroupName)]
+        private static bool IsLeapMotionAvailable()
+        {
+            return typeof(SDK_LeapMotionDefines).Assembly.GetType("Leap.Unity.LeapDeviceInfo") != null;
+        }
+    }
+}
