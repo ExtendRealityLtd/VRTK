d7c34273f44c09fd2a383a47e380cf5f156b45a7
Thomas Ascioglu <thomas.ascioglu@gmx.de>
2017-02-08 15:20:23 +0100

    leap motion integration

---
+++ VRTK/SDK/LeapMotion/SDK_LeapMotionController.cs
@@ -0,0 +1,556 @@
+﻿// LeapMotion System|SDK_LeapMotion|001
+namespace VRTK
+{
+#if VRTK_DEFINE_SDK_LEAPMOTION
+    using UnityEngine;
+    using System.Collections.Generic;
+    using System;
+    using Leap.Unity;
+#endif
+
+    /// <summary>
+    /// The LeapMotion Controller SDK script provides a bridge to SDK methods that deal with the input devices.
+    /// </summary>
+    [SDK_Description("LeapMotion", SDK_LeapMotionDefines.ScriptingDefineSymbol)]
+    public class SDK_LeapMotionController
+#if VRTK_DEFINE_SDK_LEAPMOTION
+        : SDK_BaseController
+#else
+        : SDK_FallbackController
+#endif
+    {
+#if VRTK_DEFINE_SDK_LEAPMOTION
+        private const uint leftHandControllerIndex = 0;
+        private const uint rightHandControllerIndex = 1;
+        private IHandModel leftHand;
+        private IHandModel rightHand;
+        private PinchDetector leftPinchDetector;
+        private PinchDetector rightPinchDetector;
+        private Rigidbody leftHandRigidBody;
+        private Rigidbody rightHandRigidBody;
+
+        private IHandModel LeftHand
+        {
+            get
+            {
+                if (rightHand == null)
+                {
+                    GameObject rightController = GetControllerRightHand(true);
+                    rightHand = rightController.GetComponent<IHandModel>();
+                }
+                return rightHand;
+            }
+        }
+        private IHandModel RightHand
+        {
+            get
+            {
+                if (rightHand == null)
+                {
+                    GameObject rightController = GetControllerRightHand(true);
+                    rightHand = rightController.GetComponent<IHandModel>();
+                }
+                return rightHand;
+            }
+        }
+        private PinchDetector LeftPinchDetector
+        {
+            get
+            {
+                if (leftPinchDetector == null)
+                {
+                    GameObject leftController = GetControllerLeftHand(true);
+                    leftPinchDetector = leftController.GetComponent<PinchDetector>();
+                }
+                return leftPinchDetector;
+            }
+        }
+        private PinchDetector RightPinchDetector
+        {
+            get
+            {
+                if (rightPinchDetector == null)
+                {
+                    GameObject rightController = GetControllerRightHand(true);
+                    rightPinchDetector = rightController.GetComponent<PinchDetector>();
+                }
+                return rightPinchDetector;
+            }
+        }
+        private Rigidbody LeftHandRigidBody
+        {
+            get
+            {
+                if(leftHandRigidBody == null)
+                {
+                    GameObject leftController = GetControllerLeftHand(true);
+                    leftHandRigidBody = leftController.GetComponent<Rigidbody>();
+                    if(leftHandRigidBody == null)
+                    {
+                        leftHandRigidBody = leftController.AddComponent<Rigidbody>();
+                        leftHandRigidBody.useGravity = false;
+                        leftHandRigidBody.isKinematic = true;
+                    }
+                }
+                return leftHandRigidBody;
+            }
+        }
+        private Rigidbody RightHandRigidBody
+        {
+            get
+            {
+                if (rightHandRigidBody == null)
+                {
+                    GameObject rightController = GetControllerRightHand(true);
+                    rightHandRigidBody = rightController.GetComponent<Rigidbody>();
+                    if (rightHandRigidBody == null)
+                    {
+                        rightHandRigidBody = rightController.AddComponent<Rigidbody>();
+                        rightHandRigidBody.useGravity = false;
+                        rightHandRigidBody.isKinematic = true;
+                    }
+                }
+                return rightHandRigidBody;
+            }
+        }
+
+        private ControllerHand LeapChiralityToControllerHand(Chirality handedness)
+        {
+            switch (handedness)
+            {
+                case Chirality.Left:
+                    return ControllerHand.Left;
+                case Chirality.Right:
+                    return ControllerHand.Right;
+                default:
+                    return ControllerHand.None;
+            }
+        }
+
+        private ControllerHand indexToControllerHand(uint index)
+        {
+            switch (index)
+            {
+                case leftHandControllerIndex:
+                    return ControllerHand.Left;
+                case rightHandControllerIndex:
+                    return ControllerHand.Right;
+                default:
+                    return ControllerHand.None;
+            }
+        }
+
+        private PinchDetector GetPinchDetectorByIndex(uint index)
+        {
+            switch (indexToControllerHand(index))
+            {
+                case ControllerHand.Left:
+                    return LeftPinchDetector;
+                case ControllerHand.Right:
+                    return RightPinchDetector;
+                default:
+                    return null;
+            }
+        }
+
+        private Rigidbody GetRigidBodByIndex(uint index)
+        {
+            switch (indexToControllerHand(index))
+            {
+                case ControllerHand.Left:
+                    return LeftHandRigidBody;
+                case ControllerHand.Right:
+                    return RightHandRigidBody;
+                default:
+                    return null;
+            }
+        }
+
+        private GameObject GetController(ControllerHand controllerHandedness, bool actual)
+        {
+            GameObject returnController;
+            if (controllerHandedness == ControllerHand.Right)
+            {
+                returnController = GetSDKManagerControllerRightHand(actual);
+            }
+            else
+            {
+                returnController = GetSDKManagerControllerLeftHand(actual);
+            }
+
+            if (!returnController && actual)
+            {
+                IHandModel[] handModels = FindObjectsOfType<IHandModel>();
+                foreach (IHandModel handModel in handModels)
+                {
+                    if (controllerHandedness == LeapChiralityToControllerHand(handModel.Handedness))
+                    {
+                        return handModel.gameObject;
+                    }
+                }
+            }
+            return returnController;
+        }
+
+        public override GameObject GetControllerByIndex(uint index, bool actual = false)
+        {
+            switch (indexToControllerHand(index))
+            {
+                case ControllerHand.Left:
+                    return GetControllerLeftHand(actual);
+                case ControllerHand.Right:
+                    return GetControllerRightHand(actual);
+                default:
+                    return null;
+            }
+        }
+
+        public override string GetControllerDefaultColliderPath(ControllerHand hand)
+        {
+            // TODO
+            return "ControllerColliders/Fallback";
+        }
+
+        public override uint GetControllerIndex(GameObject controller)
+        {
+            bool isLeftHand = CheckActualOrScriptAliasControllerIsLeftHand(controller);
+            if (isLeftHand)
+            {
+                return leftHandControllerIndex;
+            } else
+            {
+                return rightHandControllerIndex;
+            }
+        }
+
+        public override GameObject GetControllerLeftHand(bool actual = false)
+        {
+            return GetController(ControllerHand.Left, actual);
+        }
+
+        public override GameObject GetControllerModel(ControllerHand hand)
+        {
+            var model = GetSDKManagerControllerModelForHand(hand);
+            if (!model)
+            {
+                model = GetController(hand, true);
+            }
+            return model;
+        }
+
+        public override GameObject GetControllerModel(GameObject controller)
+        {
+            return GetControllerModelFromController(controller);
+        }
+
+        public override Transform GetControllerOrigin(GameObject controller)
+        {
+            return GetActualController(controller).transform;
+        }
+
+        public override GameObject GetControllerRenderModel(GameObject controller)
+        {
+            // TODO
+            return null;
+        }
+
+        public override GameObject GetControllerRightHand(bool actual = false)
+        {
+            return GetController(ControllerHand.Right, actual);
+        }
+
+        public override Vector3 GetVelocityOnIndex(uint index)
+        {
+            //FIXME Rigidbody velocity is always 0
+            Rigidbody rB = GetRigidBodByIndex(index);
+            return rB.velocity;
+        }
+
+        public override Vector3 GetAngularVelocityOnIndex(uint index)
+        {
+            // FIXME Rigidbody angular velocity is always 0
+            Rigidbody rB = GetRigidBodByIndex(index);
+            return rB.angularVelocity;
+        }
+
+        public override bool IsControllerLeftHand(GameObject controller)
+        {
+            return CheckActualOrScriptAliasControllerIsLeftHand(controller);
+        }
+
+        public override bool IsControllerLeftHand(GameObject controller, bool actual)
+        {
+            return CheckControllerLeftHand(controller, actual);
+        }
+
+        public override bool IsControllerRightHand(GameObject controller)
+        {
+            return CheckActualOrScriptAliasControllerIsRightHand(controller);
+        }
+
+        public override bool IsControllerRightHand(GameObject controller, bool actual)
+        {
+            return CheckControllerRightHand(controller, actual);
+        }
+
+        public override bool IsGripPressedDownOnIndex(uint index)
+        {
+            return GetPinchDetectorByIndex(index).DidStartPinch;
+        }
+
+        public override bool IsGripPressedOnIndex(uint index)
+        {
+            return GetPinchDetectorByIndex(index).IsPinching;
+        }
+
+        public override bool IsGripPressedUpOnIndex(uint index)
+        {
+            return GetPinchDetectorByIndex(index).DidEndPinch;
+        }
+
+        public override void ProcessUpdate(uint index, Dictionary<string, object> options)
+        {
+            //TODO
+        }
+        
+        #region Not Applicable
+        public override string GetControllerElementPath(ControllerElements element, ControllerHand hand, bool fullPath = false)
+        {
+            return null;
+        }
+
+        public override Vector2 GetGripAxisOnIndex(uint index)
+        {
+            // TODO
+            return Vector2.zero;
+        }
+
+        public override float GetGripHairlineDeltaOnIndex(uint index)
+        {
+            return 0;
+        }
+
+        public override SDK_ControllerHapticModifiers GetHapticModifiers()
+        {
+            return null;
+        }
+
+        public override Vector2 GetTouchpadAxisOnIndex(uint index)
+        {
+            return Vector2.zero;
+        }
+
+        public override Vector2 GetTriggerAxisOnIndex(uint index)
+        {
+            return Vector2.zero;
+        }
+
+        public override float GetTriggerHairlineDeltaOnIndex(uint index)
+        {
+            return 0;
+        }
+
+        public override void HapticPulseOnIndex(uint index, float strength = 0.5F)
+        {
+            // no haptic pulse
+        }
+
+        public override bool IsButtonOnePressedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonOnePressedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonOnePressedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonOneTouchedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonOneTouchedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonOneTouchedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonTwoPressedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonTwoPressedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonTwoPressedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonTwoTouchedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonTwoTouchedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsButtonTwoTouchedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsHairGripDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsHairGripUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsHairTriggerDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsHairTriggerUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsStartMenuPressedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsStartMenuPressedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsStartMenuPressedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsStartMenuTouchedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsStartMenuTouchedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsStartMenuTouchedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTouchpadPressedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTouchpadPressedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTouchpadPressedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTouchpadTouchedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTouchpadTouchedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTouchpadTouchedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsGripTouchedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsGripTouchedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsGripTouchedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTriggerPressedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTriggerPressedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTriggerPressedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTriggerTouchedDownOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTriggerTouchedOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override bool IsTriggerTouchedUpOnIndex(uint index)
+        {
+            return false;
+        }
+
+        public override void SetControllerRenderModelWheel(GameObject renderModel, bool state)
+        {
+            // no wheel
+        }
+
+        public override Transform GenerateControllerPointerOrigin(GameObject parent)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+        #endregion Not Applicable
+#endif
+    }
+}
