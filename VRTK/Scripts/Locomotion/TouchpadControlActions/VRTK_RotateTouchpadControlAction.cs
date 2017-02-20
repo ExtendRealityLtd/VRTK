5224e6613da263010558313f5bb9cb1d4ae225db
Benedikt Reiser <benedikt.reiser@gmail.com>
2017-02-20 15:05:03 +0100

    updated VRTK up to master commit 8d796fa9424e865db2928122bd0fd243ab87393c
    - see https://github.com/thestonefox/VRTK/commit/8d796fa9424e865db2928122bd0fd243ab87393c

---
+++ VRTK/Scripts/Locomotion/TouchpadControlActions/VRTK_RotateTouchpadControlAction.cs
@@ -0,0 +1,38 @@
+﻿// Rotate Touchpad Control Action|TouchpadControlActions|25020
+namespace VRTK
+{
+    using UnityEngine;
+
+    /// <summary>
+    /// The Rotate Touchpad Control Action script is used to rotate the controlled GameObject around the up vector when changing the touchpad axis.
+    /// </summary>
+    /// <remarks>
+    /// The effect is a smooth rotation to simulate turning.
+    /// </remarks>
+    /// <example>
+    /// `VRTK/Examples/017_CameraRig_TouchpadWalking` has a collection of walls and slopes that can be traversed by the user with the touchpad. There is also an area that can only be traversed if the user is crouching.
+    ///
+    /// To enable the Rotate Touchpad Control Action, ensure one of the `TouchpadControlOptions` children (located under the Controller script alias) has the `Rotate` control script active.
+    /// </example>
+    public class VRTK_RotateTouchpadControlAction : VRTK_BaseTouchpadControlAction
+    {
+        [Tooltip("The maximum speed the controlled object can be rotated based on the position of the touchpad axis.")]
+        public float maximumRotationSpeed = 3f;
+        [Tooltip("The rotation multiplier to be applied when the modifier button is pressed.")]
+        public float rotationMultiplier = 1.5f;
+
+        protected override void Process(GameObject controlledGameObject, Transform directionDevice, Vector3 axisDirection, float axis, float deadzone, bool currentlyFalling, bool modifierActive)
+        {
+            float angle = Rotate(axis, modifierActive);
+            if (angle != 0f)
+            {
+                RotateAroundPlayer(controlledGameObject, angle);
+            }
+        }
+
+        protected virtual float Rotate(float axis, bool modifierActive)
+        {
+            return axis * maximumRotationSpeed * Time.deltaTime * (modifierActive ? rotationMultiplier : 1) * 10;
+        }
+    }
+}
---
+++ VRTK/Scripts/Utilities/UnityEvents/VRTK_TouchpadControl_UnityEvents.cs
@@ -0,0 +1,66 @@
+﻿namespace VRTK.UnityEventHelper
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    [RequireComponent(typeof(VRTK_TouchpadControl))]
+    public class VRTK_TouchpadControl_UnityEvents : MonoBehaviour
+    {
+        private VRTK_TouchpadControl tc;
+
+        [System.Serializable]
+        public class UnityObjectEvent : UnityEvent<object, TouchpadControlEventArgs> { };
+
+        /// <summary>
+        /// Emits the XAxisChanged class event.
+        /// </summary>
+        public UnityObjectEvent OnXAxisChanged = new UnityObjectEvent();
+
+        /// <summary>
+        /// Emits the YAxisChanged class event.
+        /// </summary>
+        public UnityObjectEvent OnYAxisChanged = new UnityObjectEvent();
+
+        private void SetTouchpadControl()
+        {
+            if (tc == null)
+            {
+                tc = GetComponent<VRTK_TouchpadControl>();
+            }
+        }
+
+        private void OnEnable()
+        {
+            SetTouchpadControl();
+            if (tc == null)
+            {
+                Debug.LogError("The VRTK_TouchpadControl_UnityEvents script requires to be attached to a GameObject that contains a VRTK_TouchpadControl script");
+                return;
+            }
+
+            tc.XAxisChanged += XAxisChanged;
+            tc.YAxisChanged += YAxisChanged;
+        }
+
+        private void XAxisChanged(object o, TouchpadControlEventArgs e)
+        {
+            OnXAxisChanged.Invoke(o, e);
+        }
+
+        private void YAxisChanged(object o, TouchpadControlEventArgs e)
+        {
+            OnYAxisChanged.Invoke(o, e);
+        }
+
+        private void OnDisable()
+        {
+            if (tc == null)
+            {
+                return;
+            }
+
+            tc.XAxisChanged -= XAxisChanged;
+            tc.YAxisChanged -= YAxisChanged;
+        }
+    }
+}
