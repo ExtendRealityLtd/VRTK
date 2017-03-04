34d71dbb35ac115b6c6d9358238f735d90dc0030
Daniel Friesen <d@danf.ca>
2017-03-04 12:52:19 -0800

    chore(Simulator): switch input sim from .FindChild to Transform.Find
    Transform.FindChild is no longer documented, in effect deprecated, and
    is supposed to function the same as Transform.Find.

--- Assets/VRTK/SDK/Simulator/SDK_InputSimulator.cs
+++ Assets/VRTK/SDK/Simulator/SDK_InputSimulator.cs
@@ -94,15 +94,15 @@ namespace VRTK
 
         private void Awake()
         {
-            rightHand = transform.FindChild("RightHand");
+            rightHand = transform.Find("RightHand");
             rightHand.gameObject.SetActive(false);
-            leftHand = transform.FindChild("LeftHand");
+            leftHand = transform.Find("LeftHand");
             leftHand.gameObject.SetActive(false);
             currentHand = rightHand;
             oldPos = Input.mousePosition;
-            myCamera = transform.FindChild("Camera");
-            leftHand.FindChild("Hand").GetComponent<Renderer>().material.color = Color.red;
-            rightHand.FindChild("Hand").GetComponent<Renderer>().material.color = Color.green;
+            myCamera = transform.Find("Camera");
+            leftHand.Find("Hand").GetComponent<Renderer>().material.color = Color.red;
+            rightHand.Find("Hand").GetComponent<Renderer>().material.color = Color.green;
             rightController = rightHand.GetComponent<SDK_ControllerSim>();
             leftController = leftHand.GetComponent<SDK_ControllerSim>();
             rightController.Selected = true;
