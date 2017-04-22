dcd69452ec2f2daade5654510263910c7ef978a1
Harvey Ball <me@harveyball.com>
2017-04-22 17:07:46 +0100

    fix(ScreenFade): ensure screen fade reaches full opacity
    The VRTK_ScreenFade script could get into a state where it should be
    at an opacity of 1f but would end up at 0.9999f which meant the quad
    wasn't completely opaque so items in the scene were still barely
    visible.
    
    This fix forces the alpha channel to 1f if it exceeds 0.98f.

--- Assets/VRTK/Scripts/Internal/VRTK_ScreenFade.cs
+++ Assets/VRTK/Scripts/Internal/VRTK_ScreenFade.cs
@@ -55,6 +55,7 @@
 
             if (currentColor.a > 0 && fadeMaterial)
             {
+                currentColor.a = (currentColor.a > 0.98f ? 1f : currentColor.a);
                 fadeMaterial.color = currentColor;
                 fadeMaterial.SetPass(0);
                 GL.PushMatrix();
