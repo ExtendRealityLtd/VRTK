7b6f341e92441cc6bb8e0759b1aaf5b43494cd7c
Harvey Ball <me@harveyball.com>
2017-07-10 23:33:00 +0100

    feat(SDK): implement internal Unity VR support
    Unity supports basic VR functionality without the need for any
    third party SDK. A new SDK bridge has been added that hooks into
    this internal Unity VR support so VRTK can be used for any device
    that Unity supports.
    
    The InputManager has been update to have the default axis information
    required for the axes on the controllers to work correctly.
    
    A new VelocityEstimator script has been added that enables better
    estimations of velocities of moving objects that are not rigidbodies.
    This is used to determine the velocity and angular velocity of the
    HMD and controllers when using the Unity VR SDK.
    
    The Velocity Estimator is largely based on the velocity estimator
    script from Valve's SteamVR Interaction System, so big thanks
    to Valve for this solution!

--- Assets/VRTK/SDK/Unity/SDK_UnityHeadset.cs
+++ Assets/VRTK/SDK/Unity/SDK_UnityHeadset.cs
@@ -1,55 +1,83 @@
-﻿// Base Headset|SDK_Base|005
+﻿// Unity Headset|SDK_Unity|002
 namespace VRTK
 {
     using UnityEngine;
     using System.Collections.Generic;
 
     /// <summary>
-    /// The Base Headset SDK script provides a bridge to SDK methods that deal with the VR Headset.
+    /// The Unity Headset SDK script provides a bridge to the base Unity headset support.
     /// </summary>
-    /// <remarks>
-    /// This is an abstract class to implement the interface required by all implemented SDKs.
-    /// </remarks>
-    public abstract class SDK_BaseHeadset : SDK_Base
+    [SDK_Description(typeof(SDK_UnitySystem))]
+    [SDK_Description(typeof(SDK_UnitySystem), 1)]
+    [SDK_Description(typeof(SDK_UnitySystem), 2)]
+    [SDK_Description(typeof(SDK_UnitySystem), 3)]
+    [SDK_Description(typeof(SDK_UnitySystem), 4)]
+    public class SDK_UnityHeadset : SDK_BaseHeadset
     {
-        protected Transform cachedHeadset;
-        protected Transform cachedHeadsetCamera;
+        protected VRTK_VelocityEstimator cachedHeadsetVelocityEstimator;
 
         /// <summary>
         /// The ProcessUpdate method enables an SDK to run logic for every Unity Update
         /// </summary>
         /// <param name="options">A dictionary of generic options that can be used to within the update.</param>
-        public abstract void ProcessUpdate(Dictionary<string, object> options);
+        public override void ProcessUpdate(Dictionary<string, object> options)
+        {
+        }
 
         /// <summary>
         /// The ProcessFixedUpdate method enables an SDK to run logic for every Unity FixedUpdate
         /// </summary>
         /// <param name="options">A dictionary of generic options that can be used to within the fixed update.</param>
-        public abstract void ProcessFixedUpdate(Dictionary<string, object> options);
+        public override void ProcessFixedUpdate(Dictionary<string, object> options)
+        {
+        }
 
         /// <summary>
         /// The GetHeadset method returns the Transform of the object that is used to represent the headset in the scene.
         /// </summary>
         /// <returns>A transform of the object representing the headset in the scene.</returns>
-        public abstract Transform GetHeadset();
+        public override Transform GetHeadset()
+        {
+            cachedHeadset = GetSDKManagerHeadset();
+            if (cachedHeadset == null)
+            {
+                GameObject foundHeadset = VRTK_SharedMethods.FindEvenInactiveGameObject<SDK_UnityHeadsetTracker>();
+                if (foundHeadset != null)
+                {
+                    cachedHeadset = foundHeadset.transform;
+                }
+            }
+            return cachedHeadset;
+        }
 
         /// <summary>
         /// The GetHeadsetCamera method returns the Transform of the object that is used to hold the headset camera in the scene.
         /// </summary>
         /// <returns>A transform of the object holding the headset camera in the scene.</returns>
-        public abstract Transform GetHeadsetCamera();
+        public override Transform GetHeadsetCamera()
+        {
+            return GetHeadset();
+        }
 
         /// <summary>
         /// The GetHeadsetVelocity method is used to determine the current velocity of the headset.
         /// </summary>
         /// <returns>A Vector3 containing the current velocity of the headset.</returns>
-        public abstract Vector3 GetHeadsetVelocity();
+        public override Vector3 GetHeadsetVelocity()
+        {
+            SetHeadsetCaches();
+            return cachedHeadsetVelocityEstimator.GetVelocityEstimate();
+        }
 
         /// <summary>
         /// The GetHeadsetAngularVelocity method is used to determine the current angular velocity of the headset.
         /// </summary>
         /// <returns>A Vector3 containing the current angular velocity of the headset.</returns>
-        public abstract Vector3 GetHeadsetAngularVelocity();
+        public override Vector3 GetHeadsetAngularVelocity()
+        {
+            SetHeadsetCaches();
+            return cachedHeadsetVelocityEstimator.GetAngularVelocityEstimate();
+        }
 
         /// <summary>
         /// The HeadsetFade method is used to apply a fade to the headset camera to progressively change the colour.
@@ -57,30 +85,40 @@ namespace VRTK
         /// <param name="color">The colour to fade to.</param>
         /// <param name="duration">The amount of time the fade should take to reach the given colour.</param>
         /// <param name="fadeOverlay">Determines whether to use an overlay on the fade.</param>
-        public abstract void HeadsetFade(Color color, float duration, bool fadeOverlay = false);
+        public override void HeadsetFade(Color color, float duration, bool fadeOverlay = false)
+        {
+            VRTK_ScreenFade.Start(color, duration);
+        }
 
         /// <summary>
         /// The HasHeadsetFade method checks to see if the given game object (usually the camera) has the ability to fade the viewpoint.
         /// </summary>
         /// <param name="obj">The Transform to check to see if a camera fade is available on.</param>
         /// <returns>Returns true if the headset has fade functionality on it.</returns>
-        public abstract bool HasHeadsetFade(Transform obj);
+        public override bool HasHeadsetFade(Transform obj)
+        {
+            return (obj.GetComponentInChildren<VRTK_ScreenFade>());
+        }
 
         /// <summary>
         /// The AddHeadsetFade method attempts to add the fade functionality to the game object with the camera on it.
         /// </summary>
         /// <param name="camera">The Transform to with the camera on to add the fade functionality to.</param>
-        public abstract void AddHeadsetFade(Transform camera);
-
-        protected Transform GetSDKManagerHeadset()
+        public override void AddHeadsetFade(Transform camera)
         {
-            var sdkManager = VRTK_SDKManager.instance;
-            if (sdkManager != null && sdkManager.loadedSetup != null && sdkManager.loadedSetup.actualHeadset != null)
+            if (camera != null && !camera.GetComponent<VRTK_ScreenFade>())
             {
-                cachedHeadset = (sdkManager.loadedSetup.actualHeadset ? sdkManager.loadedSetup.actualHeadset.transform : null);
-                return cachedHeadset;
+                camera.gameObject.AddComponent<VRTK_ScreenFade>();
             }
-            return null;
+        }
+
+        protected virtual void SetHeadsetCaches()
+        {
+            Transform currentHeadset = GetHeadset();
+            if (cachedHeadsetVelocityEstimator == null && currentHeadset != null)
+            {
+                cachedHeadsetVelocityEstimator = currentHeadset.GetComponent<VRTK_VelocityEstimator>();
+            }
         }
     }
 }
--- GETTING_STARTED.md
+++ GETTING_STARTED.md
@@ -37,6 +37,19 @@ The SDK Manager allows switching the used SDK Setup at runtime. To add a simple 
 
 Read the rest of this document for more detailed instructions on how to set up and use the supported VR SDKs.
 
+### UnityEngine.VR
+
+<details><summary>**Instructions for using the built in Unity3d VR support**</summary>
+
+* Follow the initial steps above by adding the `[UnityBase_CameraRig]` prefab from `VRTK/Prefabs` as a child of the SDK Setup game object.
+* The Unity Input Manager must also be configured to utilise the axes on the VR controller.
+* Open up the Unity Input Manager tab `Edit -> Project Settings -> Input`.
+* Increase the size of the `Axes` array to the number of required new axes (usually increase by 8, 2 for trigger, 2 for grip and 4 for touchpad [x,y] which is for both hands).
+* For each new axis created, enter a name for the axis that corresponds to the name specified on the `SDK_UnityControllerTrackerScript` which is found on `[UnityBase_CameraRig/<xxxx>HandAnchor]`.
+* The axis mapping information can be found in the [Unity3d Docs](https://docs.unity3d.com/Manual/OpenVRControllers.html).
+
+</details>
+
 ### VR Simulator
 
 <details><summary>**Instructions for using the VR Simulator**</summary>
