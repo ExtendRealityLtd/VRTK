3027e7f7457b6106b9981665169a3197bcfbd2ed
Harvey Ball <me@harveyball.com>
2017-10-04 13:45:59 +0100

    feat(Locomotion): add tunnel overlay camera effect for comfort options
    The Tunnel Overlay script creates a texture overlay that can increase
    in it's radius towards the centre of the screen to reduce the perceived
    field of view.
    
    This can be triggered based on the movement and rotation of the play
    area to reduce nausea with artificial locomotion. The tunnel effect
    can also be applied when stationary to simulate effects such as damage
    being taken.
    
    Thanks to @reznovVR for the original port of the excellent
    UnityVRTunnelling repo by @sixways:
    
    https://github.com/SixWays/UnityVrTunnelling

---
+++ Assets/VRTK/Source/Scripts/Internal/VRTK_TunnelEffect.cs
@@ -0,0 +1,19 @@
+﻿namespace VRTK
+{
+    using UnityEngine;
+
+    public class VRTK_TunnelEffect : MonoBehaviour
+    {
+        protected Material material;
+
+        public virtual void SetMaterial(Material material)
+        {
+            this.material = material;
+        }
+
+        protected virtual void OnRenderImage(RenderTexture src, RenderTexture dest)
+        {
+            Graphics.Blit(src, dest, material);
+        }
+    }
+}
