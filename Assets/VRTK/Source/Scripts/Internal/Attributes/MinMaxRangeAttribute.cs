fc797d0990175eb0e524b99e1252f25fd8f05c23
Harvey Ball <me@harveyball.com>
2017-11-07 22:32:40 +0000

    fix(Internal): add editor datatypes and attributes to VRTK namespace
    To prevent name collisions with 3rd party scripts, the editor datatypes
    and attributes used by VRTK are now within the VRTK namespace.
    
    The Serialized Property Extensions script is also now included in the
    VRTK namespace in case 3rd party scripts are also using this same
    script causing it to be in the project twice.

--- Assets/VRTK/Source/Scripts/Internal/Attributes/MinMaxRangeAttribute.cs
+++ Assets/VRTK/Source/Scripts/Internal/Attributes/MinMaxRangeAttribute.cs
@@ -1,13 +1,16 @@
-﻿using UnityEngine;
-
-public class MinMaxRangeAttribute : PropertyAttribute
+﻿namespace VRTK
 {
-    public readonly float max;
-    public readonly float min;
+    using UnityEngine;
 
-    public MinMaxRangeAttribute(float min, float max)
+    public class MinMaxRangeAttribute : PropertyAttribute
     {
-        this.min = min;
-        this.max = max;
+        public readonly float max;
+        public readonly float min;
+
+        public MinMaxRangeAttribute(float min, float max)
+        {
+            this.min = min;
+            this.max = max;
+        }
     }
 }
--- Assets/VRTK/Source/Scripts/Internal/DataTypes/Limits2D.cs
+++ Assets/VRTK/Source/Scripts/Internal/DataTypes/Limits2D.cs
@@ -1,38 +1,41 @@
-﻿using UnityEngine;
-
-[System.Serializable]
-public class Limits2D
+﻿namespace VRTK
 {
-    public float minimum;
-    public float maximum;
+    using UnityEngine;
 
-    public static Limits2D zero
+    [System.Serializable]
+    public class Limits2D
     {
-        get
+        public float minimum;
+        public float maximum;
+
+        public static Limits2D zero
         {
-            return new Limits2D(0f, 0f);
+            get
+            {
+                return new Limits2D(0f, 0f);
+            }
         }
-    }
 
-    public Limits2D(float min, float max)
-    {
-        minimum = min;
-        maximum = max;
-    }
+        public Limits2D(float min, float max)
+        {
+            minimum = min;
+            maximum = max;
+        }
 
-    public Limits2D(Vector2 limits)
-    {
-        minimum = limits.x;
-        maximum = limits.y;
-    }
+        public Limits2D(Vector2 limits)
+        {
+            minimum = limits.x;
+            maximum = limits.y;
+        }
 
-    public bool WithinLimits(float value)
-    {
-        return (value >= minimum && value <= maximum);
-    }
+        public bool WithinLimits(float value)
+        {
+            return (value >= minimum && value <= maximum);
+        }
 
-    public Vector2 AsVector2()
-    {
-        return new Vector2(minimum, maximum);
+        public Vector2 AsVector2()
+        {
+            return new Vector2(minimum, maximum);
+        }
     }
 }
