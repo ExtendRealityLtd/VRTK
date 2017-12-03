ExtendRealityLtd/VRTK

commit c40d0d8f8e91226430615e10765e55763d12d8e3

refactor(Structure): provide helper methods for lists and dictionaries
There are a number of places where the use of a List or Dictionary has
unnecessary repetition in checking if a value or key is present before
either adding or removing from the list or dictionary.

There are also times where a value is attempted to be retrieved from
a dictionary but it doesn't exist which would then need to be added
so multiple dictionary lookups are done, which is inefficient. The
proposed changes ensure a single look up is done via `TryGetValue`
which will reduce dictionary usage overhead.

Some places that used lists and cared about not having duplicates but
didn't care about list order have been updated to use a `HashSet`
which by default doesn't allow duplicates so there is no need for
an unnecessary `Contains` check. Some scripts iterated through
the `List` with a `for` loop which is not possible with a `HashSet`
so they have been replaced with a `foreach` loop as this shouldn't
cause any efficiency issues in Unity 5.6 and above.

Assets/VRTK/Source/Scripts/Interactions/Highlighters/VRTK_MaterialPropertyBlockColorSwapHighlighter.cs
@@ -32,8 +32,8 @@ namespace VRTK.Highlighters
         public override void Initialise(Color? color = null, GameObject affectObject = null, Dictionary<string, object> options = null)
         {
             objectToAffect = (affectObject != null ? affectObject : gameObject);
-            originalMaterialPropertyBlocks = new Dictionary<string, MaterialPropertyBlock>();
-            highlightMaterialPropertyBlocks = new Dictionary<string, MaterialPropertyBlock>();
+            originalMaterialPropertyBlocks.Clear();
+            highlightMaterialPropertyBlocks.Clear();
             // call to parent highlighter
             base.Initialise(color, affectObject, options);
         }
@@ -45,7 +45,7 @@ namespace VRTK.Highlighters
         /// <param name="duration">Not used.</param>
         public override void Unhighlight(Color? color = null, float duration = 0f)
         {
-            if (originalMaterialPropertyBlocks == null || objectToAffect == null)
+            if (objectToAffect == null)
             {
                 return;
             }
@@ -64,12 +64,12 @@ namespace VRTK.Highlighters
             {
                 Renderer renderer = renderers[i];
                 string objectReference = renderer.gameObject.GetInstanceID().ToString();
-                if (!originalMaterialPropertyBlocks.ContainsKey(objectReference))
+                MaterialPropertyBlock storedPropertyBlock = VRTK_SharedMethods.GetDictionaryValue(originalMaterialPropertyBlocks, objectReference);
+                if (storedPropertyBlock == null)
                 {
                     continue;
                 }
-
-                renderer.SetPropertyBlock(originalMaterialPropertyBlocks[objectReference]);
+                renderer.SetPropertyBlock(storedPropertyBlock);
             }
         }
 
@@ -82,9 +82,10 @@ namespace VRTK.Highlighters
             {
                 Renderer renderer = renderers[i];
                 string objectReference = renderer.gameObject.GetInstanceID().ToString();
-                originalMaterialPropertyBlocks[objectReference] = new MaterialPropertyBlock();
-                renderer.GetPropertyBlock(originalMaterialPropertyBlocks[objectReference]);
-                highlightMaterialPropertyBlocks[objectReference] = new MaterialPropertyBlock();
+                MaterialPropertyBlock blankPropertyBlock = new MaterialPropertyBlock();
+                VRTK_SharedMethods.AddDictionaryValue(originalMaterialPropertyBlocks, objectReference, blankPropertyBlock, true);
+                VRTK_SharedMethods.AddDictionaryValue(highlightMaterialPropertyBlocks, objectReference, blankPropertyBlock, true);
+                renderer.GetPropertyBlock(blankPropertyBlock);
             }
         }
 
@@ -94,19 +95,20 @@ namespace VRTK.Highlighters
             for (int i = 0; i < renderers.Length; i++)
             {
                 Renderer renderer = renderers[i];
-                string objectReference = renderer.gameObject.GetInstanceID().ToString();
-                if (!originalMaterialPropertyBlocks.ContainsKey(objectReference))
+                string faderRoutineID = renderer.gameObject.GetInstanceID().ToString();
+                if (VRTK_SharedMethods.GetDictionaryValue(originalMaterialPropertyBlocks, faderRoutineID) == null)
                 {
                     continue;
                 }
 
-                if (faderRoutines.ContainsKey(objectReference) && faderRoutines[objectReference] != null)
+                Coroutine existingFaderRoutine = VRTK_SharedMethods.GetDictionaryValue(faderRoutines, faderRoutineID);
+                if (existingFaderRoutine != null)
                 {
-                    StopCoroutine(faderRoutines[objectReference]);
-                    faderRoutines.Remove(objectReference);
+                    StopCoroutine(existingFaderRoutine);
+                    faderRoutines.Remove(faderRoutineID);
                 }
 
-                MaterialPropertyBlock highlightMaterialPropertyBlock = highlightMaterialPropertyBlocks[objectReference];
+                MaterialPropertyBlock highlightMaterialPropertyBlock = highlightMaterialPropertyBlocks[faderRoutineID];
                 renderer.GetPropertyBlock(highlightMaterialPropertyBlock);
                 if (resetMainTexture)
                 {
@@ -115,7 +117,7 @@ namespace VRTK.Highlighters
 
                 if (duration > 0f)
                 {
-                    faderRoutines[objectReference] = StartCoroutine(CycleColor(renderer, highlightMaterialPropertyBlock, color, duration));
+                    VRTK_SharedMethods.AddDictionaryValue(faderRoutines, faderRoutineID, StartCoroutine(CycleColor(renderer, highlightMaterialPropertyBlock, color, duration)), true);
                 }
                 else
                 {

Assets/VRTK/Prefabs/RadialMenu/VRTK_IndependentRadialMenuController.cs
@@ -37,8 +37,8 @@ namespace VRTK
         [Tooltip("The object the RadialMenu should face towards. If left empty, it will automatically try to find the Headset Camera.")]
         public GameObject rotateTowards;
 
-        protected List<GameObject> interactingObjects; // Objects (controllers) that are either colliding with the menu or clicking the menu
-        protected List<GameObject> collidingObjects; // Just objects that are currently colliding with the menu or its parent
+        protected List<GameObject> interactingObjects = new List<GameObject>(); // Objects (controllers) that are either colliding with the menu or clicking the menu
+        protected HashSet<GameObject> collidingObjects = new HashSet<GameObject>(); // Just objects that are currently colliding with the menu or its parent
         protected SphereCollider menuCollider;
         protected Coroutine delayedSetColliderEnabledRoutine;
         protected Vector3 desiredColliderCenter;
@@ -87,8 +87,8 @@ namespace VRTK
             }
 
             // Reset variables
-            interactingObjects = new List<GameObject>();
-            collidingObjects = new List<GameObject>();
+            interactingObjects.Clear();
+            collidingObjects.Clear();
             if (delayedSetColliderEnabledRoutine != null)
             {
                 StopCoroutine(delayedSetColliderEnabledRoutine);
@@ -262,8 +262,7 @@ namespace VRTK
         {
             DoShowMenu(CalculateAngle(e.interactingObject), sender);
             collidingObjects.Add(e.interactingObject);
-
-            interactingObjects.Add(e.interactingObject);
+            VRTK_SharedMethods.AddListValue(interactingObjects, e.interactingObject, true);
             if (addMenuCollider && menuCollider != null)
             {
                 SetColliderState(true, e);
@@ -280,7 +279,6 @@ namespace VRTK
             if (((!menu.executeOnUnclick || !isClicked) && menu.hideOnRelease) || (Object)sender == this)
             {
                 DoHideMenu(hideAfterExecution, sender);
-
                 interactingObjects.Remove(e.interactingObject);
                 if (addMenuCollider && menuCollider != null)
                 {

Assets/VRTK/Prefabs/RadialMenu/VRTK_RadialMenu.cs
@@ -59,7 +59,7 @@ namespace VRTK
         }
 
         [Tooltip("An array of Buttons that define the interactive buttons required to be displayed as part of the radial menu.")]
-        public List<RadialMenuButton> buttons;
+        public List<RadialMenuButton> buttons = new List<RadialMenuButton>();
         [Tooltip("The base for each button in the menu, by default set to a dynamic circle arc that will fill up a portion of the menu.")]
         public GameObject buttonPrefab;
         [Tooltip("If checked, then the buttons will be auto generated on awake.")]
@@ -95,7 +95,7 @@ namespace VRTK
 
         //Has to be public to keep state from editor -> play mode?
         [Tooltip("The actual GameObjects that make up the radial menu.")]
-        public List<GameObject> menuButtons;
+        public List<GameObject> menuButtons = new List<GameObject>();
 
         protected int currentHover = -1;
         protected int currentPress = -1;
@@ -295,8 +295,7 @@ namespace VRTK
                         buttonIcon.transform.eulerAngles = GetComponentInParent<Canvas>().transform.eulerAngles;
                     }
                 }
-                menuButtons.Add(newButton);
-
+                VRTK_SharedMethods.AddListValue(menuButtons, newButton, true);
             }
         }
 
@@ -306,7 +305,7 @@ namespace VRTK
         /// <param name="newButton">The button to add.</param>
         public void AddButton(RadialMenuButton newButton)
         {
-            buttons.Add(newButton);
+            VRTK_SharedMethods.AddListValue(buttons, newButton, true);
             RegenerateButtons();
         }
 
@@ -437,15 +436,14 @@ namespace VRTK
 
         protected virtual void RemoveAllButtons()
         {
-            if (menuButtons == null)
+            if (menuButtons != null)
             {
-                menuButtons = new List<GameObject>();
+                for (int i = 0; i < menuButtons.Count; i++)
+                {
+                    DestroyImmediate(menuButtons[i]);
+                }
+                menuButtons.Clear();
             }
-            for (int i = 0; i < menuButtons.Count; i++)
-            {
-                DestroyImmediate(menuButtons[i]);
-            }
-            menuButtons = new List<GameObject>();
         }
     }
 }

Assets/VRTK/Prefabs/SnapDropZone/VRTK_SnapDropZone.cs
@@ -284,7 +284,7 @@ namespace VRTK
             List<GameObject> returnList = new List<GameObject>();
             for (int i = 0; i < currentValidSnapInteractableObjects.Count; i++)
             {
-                returnList.Add(currentValidSnapInteractableObjects[i].gameObject);
+                VRTK_SharedMethods.AddListValue(returnList, currentValidSnapInteractableObjects[i].gameObject);
             }
             return returnList;
         }
@@ -847,11 +847,10 @@ namespace VRTK
         {
             if (givenObject != null)
             {
-                if (!currentValidSnapInteractableObjects.Contains(givenObject))
+                if (VRTK_SharedMethods.AddListValue(currentValidSnapInteractableObjects, givenObject, true))
                 {
                     givenObject.InteractableObjectGrabbed += InteractableObjectGrabbed;
                     givenObject.InteractableObjectUngrabbed += InteractableObjectUngrabbed;
-                    currentValidSnapInteractableObjects.Add(givenObject);
                 }
             }
         }
@@ -860,11 +859,10 @@ namespace VRTK
         {
             if (givenObject != null)
             {
-                if (currentValidSnapInteractableObjects.Contains(givenObject))
+                if (currentValidSnapInteractableObjects.Remove(givenObject))
                 {
                     givenObject.InteractableObjectGrabbed -= InteractableObjectGrabbed;
                     givenObject.InteractableObjectUngrabbed -= InteractableObjectUngrabbed;
-                    currentValidSnapInteractableObjects.Remove(givenObject);
                 }
             }
         }

Assets/VRTK/Source/SDK/Simulator/SDK_SimController.cs
@@ -473,7 +473,7 @@ namespace VRTK
         /// <returns>whether or not the TouchModifier is active</returns>
         protected virtual bool IsTouchModifierPressed()
         {
-            return Input.GetKey(keyMappings["TouchModifier"]);
+            return Input.GetKey(VRTK_SharedMethods.GetDictionaryValue(keyMappings, "TouchModifier", KeyCode.None));
         }
 
         /// <summary>
@@ -484,7 +484,7 @@ namespace VRTK
         /// <returns>whether or not the HairTouchModifier is active</returns>
         protected virtual bool IsHairTouchModifierPressed()
         {
-            return Input.GetKey(keyMappings["HairTouchModifier"]);
+            return Input.GetKey(VRTK_SharedMethods.GetDictionaryValue(keyMappings, "HairTouchModifier", KeyCode.None));
         }
 
         /// <summary>
@@ -518,29 +518,30 @@ namespace VRTK
         /// <returns>Returns true if the button state matches the given data.</returns>
         protected virtual bool GetControllerButtonState(uint index, string keyMapping, ButtonPressTypes pressType)
         {
+            KeyCode buttonCode = VRTK_SharedMethods.GetDictionaryValue(keyMappings, keyMapping, KeyCode.None);
             if (pressType == ButtonPressTypes.Touch)
             {
-                return IsButtonPressed(index, ButtonPressTypes.Press, keyMappings[keyMapping]);
+                return IsButtonPressed(index, ButtonPressTypes.Press, buttonCode);
             }
             else if (pressType == ButtonPressTypes.TouchDown)
             {
-                return IsButtonPressed(index, ButtonPressTypes.PressDown, keyMappings[keyMapping]);
+                return IsButtonPressed(index, ButtonPressTypes.PressDown, buttonCode);
             }
             else if (pressType == ButtonPressTypes.TouchUp)
             {
-                return IsButtonPressed(index, ButtonPressTypes.PressUp, keyMappings[keyMapping]);
+                return IsButtonPressed(index, ButtonPressTypes.PressUp, buttonCode);
             }
             else if (pressType == ButtonPressTypes.Press)
             {
-                return !IsButtonPressIgnored() && IsButtonPressed(index, ButtonPressTypes.Press, keyMappings[keyMapping]);
+                return !IsButtonPressIgnored() && IsButtonPressed(index, ButtonPressTypes.Press, buttonCode);
             }
             else if (pressType == ButtonPressTypes.PressDown)
             {
-                return !IsButtonPressIgnored() && IsButtonPressed(index, ButtonPressTypes.PressDown, keyMappings[keyMapping]);
+                return !IsButtonPressIgnored() && IsButtonPressed(index, ButtonPressTypes.PressDown, buttonCode);
             }
             else if (pressType == ButtonPressTypes.PressUp)
             {
-                return !IsButtonPressIgnored() && IsButtonPressed(index, ButtonPressTypes.PressUp, keyMappings[keyMapping]);
+                return !IsButtonPressIgnored() && IsButtonPressed(index, ButtonPressTypes.PressUp, buttonCode);
             }
             return false;
         }

Assets/VRTK/Source/SDK/SteamVR/SDK_SteamVRController.cs
@@ -30,8 +30,8 @@ namespace VRTK
         protected Dictionary<GameObject, SteamVR_TrackedObject> cachedTrackedObjectsByGameObject = new Dictionary<GameObject, SteamVR_TrackedObject>();
         protected Dictionary<uint, SteamVR_TrackedObject> cachedTrackedObjectsByIndex = new Dictionary<uint, SteamVR_TrackedObject>();
         protected Dictionary<EVRButtonId, bool> axisTouchStates = new Dictionary<EVRButtonId, bool>();
-        protected ushort maxHapticVibration = 3999;
         protected Dictionary<EVRButtonId, float> axisTouchFidelity = new Dictionary<EVRButtonId, float>() { { EVRButtonId.k_EButton_SteamVR_Touchpad, 0f }, { EVRButtonId.k_EButton_Axis2, 0.25f } };
+        protected ushort maxHapticVibration = 3999;
 
 #if !VRTK_DEFINE_STEAMVR_PLUGIN_1_2_2_OR_NEWER
         /// <summary>
@@ -203,9 +203,10 @@ namespace VRTK
                     }
                 }
 
-                if (cachedTrackedObjectsByIndex.ContainsKey(index) && cachedTrackedObjectsByIndex[index] != null)
+                SteamVR_TrackedObject currentTrackedObjectByIndex = VRTK_SharedMethods.GetDictionaryValue(cachedTrackedObjectsByIndex, index);
+                if (currentTrackedObjectByIndex != null)
                 {
-                    return cachedTrackedObjectsByIndex[index].gameObject;
+                    return currentTrackedObjectByIndex.gameObject;
                 }
             }
 
@@ -689,17 +690,19 @@ namespace VRTK
                 return null;
             }
 
-            if (cachedTrackedObjectsByGameObject.ContainsKey(controller) && cachedTrackedObjectsByGameObject[controller] != null)
+            SteamVR_TrackedObject currentTrackedObjectByGameObject = VRTK_SharedMethods.GetDictionaryValue(cachedTrackedObjectsByGameObject, controller);
+
+            if (currentTrackedObjectByGameObject != null)
             {
-                return cachedTrackedObjectsByGameObject[controller];
+                return currentTrackedObjectByGameObject;
             }
             else
             {
                 SteamVR_TrackedObject trackedObject = controller.GetComponent<SteamVR_TrackedObject>();
                 if (trackedObject != null)
                 {
-                    cachedTrackedObjectsByGameObject.Add(controller, trackedObject);
-                    cachedTrackedObjectsByIndex.Add((uint)trackedObject.index, trackedObject);
+                    VRTK_SharedMethods.AddDictionaryValue(cachedTrackedObjectsByGameObject, controller, trackedObject, true);
+                    VRTK_SharedMethods.AddDictionaryValue(cachedTrackedObjectsByIndex, (uint)trackedObject.index, trackedObject, true);
                 }
                 return trackedObject;
             }
@@ -740,18 +743,9 @@ namespace VRTK
             }
             SteamVR_Controller.Device device = SteamVR_Controller.Input((int)index);
             Vector2 axisValue = device.GetAxis(axisId);
-            bool currentAxisPressState;
-            if (!axisTouchStates.TryGetValue(axisId, out currentAxisPressState))
-            {
-                axisTouchStates.Add(axisId, false);
-                currentAxisPressState = false;
-            }
 
-            float axisFidelity;
-            if (!axisTouchFidelity.TryGetValue(axisId, out axisFidelity))
-            {
-                axisFidelity = 0f;
-            }
+            bool currentAxisPressState = VRTK_SharedMethods.GetDictionaryValue(axisTouchStates, axisId, false, true);
+            float axisFidelity = VRTK_SharedMethods.GetDictionaryValue(axisTouchFidelity, axisId);
 
             switch (type)
             {
@@ -760,14 +754,14 @@ namespace VRTK
                 case ButtonPressTypes.TouchDown:
                     if (!currentAxisPressState && !VRTK_SharedMethods.Vector3ShallowCompare(axisValue, Vector2.zero, axisFidelity))
                     {
-                        axisTouchStates[axisId] = true;
+                        VRTK_SharedMethods.AddDictionaryValue(axisTouchStates, axisId, true, true);
                         return true;
                     }
                     return false;
                 case ButtonPressTypes.TouchUp:
                     if (currentAxisPressState && VRTK_SharedMethods.Vector3ShallowCompare(axisValue, Vector2.zero, axisFidelity))
                     {
-                        axisTouchStates[axisId] = false;
+                        VRTK_SharedMethods.AddDictionaryValue(axisTouchStates, axisId, false, true);
                         return true;
                     }
                     return false;

Assets/VRTK/Source/SDK/Unity/SDK_UnityController.cs
@@ -529,8 +529,9 @@ namespace VRTK
             }
 
             bool isRightController = (controllerReference.hand == ControllerHand.Right);
-            KeyCode? touchButton = (isRightController ? rightControllerTouchKeyCodes[buttonType] : leftControllerTouchKeyCodes[buttonType]);
-            KeyCode? pressButton = (isRightController ? rightControllerPressKeyCodes[buttonType] : leftControllerPressKeyCodes[buttonType]);
+
+            KeyCode? touchButton = VRTK_SharedMethods.GetDictionaryValue((isRightController ? rightControllerTouchKeyCodes : leftControllerTouchKeyCodes), buttonType);
+            KeyCode? pressButton = VRTK_SharedMethods.GetDictionaryValue((isRightController ? rightControllerPressKeyCodes : leftControllerPressKeyCodes), buttonType);
 
             switch (buttonType)
             {
@@ -603,40 +604,29 @@ namespace VRTK
             return 0f;
         }
 
+        protected virtual bool IsAxisOnHandButtonPress(Dictionary<ButtonTypes, bool> axisHandState, ButtonTypes buttonType, ButtonPressTypes pressType, Vector2 axisValue)
+        {
+            bool previousAxisState = VRTK_SharedMethods.GetDictionaryValue(axisHandState, buttonType);
+            if (pressType == ButtonPressTypes.PressDown && !previousAxisState)
+            {
+                bool currentAxisState = GetAxisPressState(previousAxisState, axisValue.x);
+                VRTK_SharedMethods.AddDictionaryValue(axisHandState, buttonType, currentAxisState, true);
+                return currentAxisState;
+            }
+            if (pressType == ButtonPressTypes.PressUp && previousAxisState)
+            {
+                bool currentAxisState = GetAxisPressState(previousAxisState, axisValue.x);
+                VRTK_SharedMethods.AddDictionaryValue(axisHandState, buttonType, currentAxisState, true);
+                return !currentAxisState;
+            }
+            return false;
+        }
+
         protected virtual bool IsAxisButtonPress(VRTK_ControllerReference controllerReference, ButtonTypes buttonType, ButtonPressTypes pressType)
         {
             bool isRightController = (controllerReference.hand == ControllerHand.Right);
             Vector2 axisValue = GetButtonAxis(buttonType, controllerReference);
-
-            if (isRightController)
-            {
-                bool previousAxisState = rightAxisButtonPressState[buttonType];
-                if (pressType == ButtonPressTypes.PressDown && !previousAxisState)
-                {
-                    rightAxisButtonPressState[buttonType] = GetAxisPressState(rightAxisButtonPressState[buttonType], axisValue.x);
-                    return rightAxisButtonPressState[buttonType];
-                }
-                if (pressType == ButtonPressTypes.PressUp && previousAxisState)
-                {
-                    rightAxisButtonPressState[buttonType] = GetAxisPressState(rightAxisButtonPressState[buttonType], axisValue.x);
-                    return !rightAxisButtonPressState[buttonType];
-                }
-            }
-            else
-            {
-                bool previousAxisState = leftAxisButtonPressState[buttonType];
-                if (pressType == ButtonPressTypes.PressDown && !previousAxisState)
-                {
-                    leftAxisButtonPressState[buttonType] = GetAxisPressState(leftAxisButtonPressState[buttonType], axisValue.x);
-                    return leftAxisButtonPressState[buttonType];
-                }
-                if (pressType == ButtonPressTypes.PressUp && previousAxisState)
-                {
-                    leftAxisButtonPressState[buttonType] = GetAxisPressState(leftAxisButtonPressState[buttonType], axisValue.x);
-                    return !leftAxisButtonPressState[buttonType];
-                }
-            }
-            return false;
+            return IsAxisOnHandButtonPress((isRightController ? rightAxisButtonPressState : leftAxisButtonPressState), buttonType, pressType, axisValue);
         }
 
         protected virtual bool GetAxisPressState(bool currentState, float axisValue)
@@ -787,15 +777,15 @@ namespace VRTK
 
         protected virtual void SetControllerButtonValues(ref Dictionary<ButtonTypes, KeyCode?> touchKeyCodes, ref Dictionary<ButtonTypes, KeyCode?> pressKeyCodes, int joystickIndex, int[] touchCodes, int[] pressCodes)
         {
-            touchKeyCodes[ButtonTypes.Trigger] = StringToKeyCode(joystickIndex, touchCodes[0]);
-            touchKeyCodes[ButtonTypes.Touchpad] = StringToKeyCode(joystickIndex, touchCodes[1]);
-            touchKeyCodes[ButtonTypes.ButtonOne] = StringToKeyCode(joystickIndex, touchCodes[2]);
-            touchKeyCodes[ButtonTypes.ButtonTwo] = StringToKeyCode(joystickIndex, touchCodes[3]);
+            VRTK_SharedMethods.AddDictionaryValue(touchKeyCodes, ButtonTypes.Trigger, StringToKeyCode(joystickIndex, touchCodes[0]), true);
+            VRTK_SharedMethods.AddDictionaryValue(touchKeyCodes, ButtonTypes.Touchpad, StringToKeyCode(joystickIndex, touchCodes[1]), true);
+            VRTK_SharedMethods.AddDictionaryValue(touchKeyCodes, ButtonTypes.ButtonOne, StringToKeyCode(joystickIndex, touchCodes[2]), true);
+            VRTK_SharedMethods.AddDictionaryValue(touchKeyCodes, ButtonTypes.ButtonTwo, StringToKeyCode(joystickIndex, touchCodes[3]), true);
 
-            pressKeyCodes[ButtonTypes.Touchpad] = StringToKeyCode(joystickIndex, pressCodes[0]);
-            pressKeyCodes[ButtonTypes.ButtonOne] = StringToKeyCode(joystickIndex, pressCodes[1]);
-            pressKeyCodes[ButtonTypes.ButtonTwo] = StringToKeyCode(joystickIndex, pressCodes[2]);
-            pressKeyCodes[ButtonTypes.StartMenu] = StringToKeyCode(joystickIndex, pressCodes[3]);
+            VRTK_SharedMethods.AddDictionaryValue(pressKeyCodes, ButtonTypes.Touchpad, StringToKeyCode(joystickIndex, pressCodes[0]), true);
+            VRTK_SharedMethods.AddDictionaryValue(pressKeyCodes, ButtonTypes.ButtonOne, StringToKeyCode(joystickIndex, pressCodes[1]), true);
+            VRTK_SharedMethods.AddDictionaryValue(pressKeyCodes, ButtonTypes.ButtonTwo, StringToKeyCode(joystickIndex, pressCodes[2]), true);
+            VRTK_SharedMethods.AddDictionaryValue(pressKeyCodes, ButtonTypes.StartMenu, StringToKeyCode(joystickIndex, pressCodes[3]), true);
         }
 
         protected virtual KeyCode StringToKeyCode(int index, int code)

Assets/VRTK/Source/Scripts/Interactions/Highlighters/VRTK_BaseHighlighter.cs
@@ -57,11 +57,7 @@ namespace VRTK.Highlighters
         /// <returns>The value in the options at the given key returned in the provided system type.</returns>
         public virtual T GetOption<T>(Dictionary<string, object> options, string key)
         {
-            if (options != null && options.ContainsKey(key) && options[key] != null)
-            {
-                return (T)options[key];
-            }
-            return default(T);
+            return (T)VRTK_SharedMethods.GetDictionaryValue(options, key, default(T));
         }
 
         /// <summary>

commit 077d8e7f9c2f9b8ca984fe3a3bc7894e961972b4

fix(Highlighter): MBP highlighter not restoring material properties
- a bug was introduced with c40d0d8f8e91226430615e10765e55763d12d8e3
  that led to materials not being correctly restored to their initial
  state when unhighlighting
- the reason is that two separate material property blocks need to
  be stored - one for restoring the intial values and one for modifying
  material properties during the highlight

Assets/VRTK/Source/Scripts/Interactions/Highlighters/VRTK_MaterialPropertyBlockColorSwapHighlighter.cs
@@ -82,10 +82,14 @@ namespace VRTK.Highlighters
             {
                 Renderer renderer = renderers[i];
                 string objectReference = renderer.gameObject.GetInstanceID().ToString();
-                MaterialPropertyBlock blankPropertyBlock = new MaterialPropertyBlock();
-                VRTK_SharedMethods.AddDictionaryValue(originalMaterialPropertyBlocks, objectReference, blankPropertyBlock, true);
-                VRTK_SharedMethods.AddDictionaryValue(highlightMaterialPropertyBlocks, objectReference, blankPropertyBlock, true);
-                renderer.GetPropertyBlock(blankPropertyBlock);
+                // get the initial material property block to restore the original material properties later on
+                MaterialPropertyBlock originalPropertyBlock = new MaterialPropertyBlock();
+                renderer.GetPropertyBlock(originalPropertyBlock);
+                VRTK_SharedMethods.AddDictionaryValue(originalMaterialPropertyBlocks, objectReference, originalPropertyBlock, true);
+                // we need a second instance of the original material property block which will be modified for highlighting
+                MaterialPropertyBlock highlightPropertyBlock = new MaterialPropertyBlock();
+                renderer.GetPropertyBlock(highlightPropertyBlock);
+                VRTK_SharedMethods.AddDictionaryValue(highlightMaterialPropertyBlocks, objectReference, highlightPropertyBlock, true);
             }
         }
 