5029ce9eee0a6bc1b5f598581c85a7f182059603
phatboysh <32048361+phatboysh@users.noreply.github.com>
2017-12-15 03:08:51 -0500

    fix(Locomotion): Continuous Rotation in DragWorld
    Changed vector math to allow for continuous rotation.

--- Assets/VRTK/Source/Scripts/Locomotion/VRTK_DragWorld.cs
+++ Assets/VRTK/Source/Scripts/Locomotion/VRTK_DragWorld.cs
@@ -386,7 +386,7 @@ namespace VRTK
             if (rotationTrackingController == TrackingController.BothControllers && VRTK_ControllerReference.IsValid(leftControllerReference) && VRTK_ControllerReference.IsValid(rightControllerReference))
             {
                 Vector2 currentRotationAngle = GetControllerRotation();
-                float newAngle = Vector2.Angle(currentRotationAngle, previousRotationAngle) * Mathf.Sign(currentRotationAngle.x * currentRotationAngle.y - previousRotationAngle.x * previousRotationAngle.y);
+                float newAngle = Vector2.Angle(currentRotationAngle, previousRotationAngle)*Mathf.Sign(Vector3.Cross(currentRotationAngle, previousRotationAngle).z);
                 RotateByAngle(newAngle);
                 previousRotationAngle = currentRotationAngle;
             }
