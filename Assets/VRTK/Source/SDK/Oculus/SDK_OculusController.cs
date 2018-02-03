77f8b7f2e10fd34df9d8320300f57d02c9f59b03
Stephen Eisenhauer <bhs2007@gmail.com>
2018-02-03 14:40:44 -0800

    Improve Oculus SDK controller detection
    Prefer OVRInput.GetConnectedControllers over GetActiveController 
    for earlier and more reliable controller type detection. 
    
    GetActiveController only reports the type of the single controller 
    whose inputs are being processed right now, whereas 
    GetConnectedControllers reports the overall controller scheme type 
    as soon as the SDK has connected to them.

--- Assets/VRTK/Source/SDK/Oculus/SDK_OculusController.cs
+++ Assets/VRTK/Source/SDK/Oculus/SDK_OculusController.cs
@@ -88,7 +88,7 @@ namespace VRTK
         /// <returns>The ControllerType based on the SDK and headset being used.</returns>
         public override ControllerType GetCurrentControllerType(VRTK_ControllerReference controllerReference = null)
         {
-            switch (OVRInput.GetActiveController())
+            switch (OVRInput.GetConnectedControllers())
             {
                 case OVRInput.Controller.LTouch:
                 case OVRInput.Controller.RTouch:
