80db756d9f0a653236e3ff6f0ce4516ea1581111
Harvey Ball <me@harveyball.com>
2018-09-26 18:55:03 +0100

    fix(Examples): rebuild oculus camera rig without a prefab
    The PrefabUtility caused so many issues when attempting to create
    a linked prefab with the camera rig fix. Instead this just uses
    instantiate which doesn't have the same issues.

--- Assets/VRTK/Examples/ExampleResources/SharedResources/Scripts/VRTKExample_FixSetup.cs
+++ Assets/VRTK/Examples/ExampleResources/SharedResources/Scripts/VRTKExample_FixSetup.cs
@@ -7,6 +7,9 @@ namespace VRTK.Examples.Utilities
     [ExecuteInEditMode]
     public class VRTKExample_FixSetup : MonoBehaviour
     {
+        public bool forceOculusFloorLevel = true;
+        protected bool trackingLevelFloor = false;
+
         public virtual void ApplyFixes()
         {
             FixOculus();
@@ -14,7 +17,36 @@ namespace VRTK.Examples.Utilities
 
         protected virtual void Awake()
         {
-            ApplyFixes();
+
+            if (Application.isEditor && !Application.isPlaying)
+            {
+                ApplyFixes();
+            }
+        }
+
+        protected virtual void Update()
+        {
+            FixTrackingType();
+        }
+
+        protected virtual void FixTrackingType()
+        {
+#if VRTK_DEFINE_SDK_OCULUS
+            if (forceOculusFloorLevel && !trackingLevelFloor)
+            {
+                GameObject overManagerGO = GameObject.Find("[VRTK_SDKManager]/[VRTK_SDKSetups]/Oculus/OVRCameraRig");
+                if (overManagerGO != null)
+                {
+                    OVRManager ovrManager = overManagerGO.GetComponent<OVRManager>();
+                    if (ovrManager != null)
+                    {
+                        ovrManager.trackingOriginType = OVRManager.TrackingOrigin.FloorLevel;
+                        trackingLevelFloor = true;
+                        Debug.Log("Forced Oculus Tracking to Floor Level");
+                    }
+                }
+            }
+#endif
         }
 
         protected virtual void FixOculus()
@@ -22,6 +54,13 @@ namespace VRTK.Examples.Utilities
 #if VRTK_DEFINE_SDK_OCULUS
             string oculusPath = "[VRTK_SDKManager]/[VRTK_SDKSetups]/Oculus";
             GameObject oculusSDK = GameObject.Find(oculusPath);
+
+            if (oculusSDK == null || oculusSDK.GetComponentInChildren<OVRManager>() != null)
+            {
+                Debug.Log("No Oculus Repaired Required");
+                return;
+            }
+
             GameObject currentRig = GameObject.Find(oculusPath + "/OVRCameraRig");
             GameObject currentAvatar = GameObject.Find(oculusPath + "/LocalAvatar");
             VRTK_SDKSetup oculusSetup = oculusSDK.GetComponent<VRTK_SDKSetup>();
@@ -35,30 +74,47 @@ namespace VRTK.Examples.Utilities
                 DestroyImmediate(currentAvatar);
             }
 
-            GameObject ovrCameraRig = PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath("Assets/Oculus/VR/Prefabs/OVRCameraRig.prefab", typeof(GameObject))) as GameObject;
+            GameObject ovrCameraRig = null;
+            GameObject ovrCameraRigToInstantiate = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Oculus/VR/Prefabs/OVRCameraRig.prefab", typeof(GameObject));
+            if (ovrCameraRigToInstantiate != null)
+            {
+                ovrCameraRig = Instantiate(ovrCameraRigToInstantiate) as GameObject;
+            }
             if (ovrCameraRig != null)
             {
+                ovrCameraRig.name = ovrCameraRig.name.Replace("(Clone)", "");
                 ovrCameraRig.transform.SetParent(oculusSDK.transform);
-                ovrCameraRig.SetActive(false);
+                ovrCameraRig.SetActive(true);
                 oculusSetup.actualBoundaries = ovrCameraRig;
                 oculusSetup.actualHeadset = GameObject.Find(oculusPath + "/OVRCameraRig/TrackingSpace/CenterEyeAnchor");
                 oculusSetup.actualLeftController = GameObject.Find(oculusPath + "/OVRCameraRig/TrackingSpace/LeftHandAnchor");
                 oculusSetup.actualRightController = GameObject.Find(oculusPath + "/OVRCameraRig/TrackingSpace/RightHandAnchor");
-                OVRManager ovrManager = ovrCameraRig.GetComponent<OVRManager>();
-                ovrManager.trackingOriginType = OVRManager.TrackingOrigin.FloorLevel;
                 Debug.Log("Successfully repaired Oculus OVRCameraRig prefab");
             }
 
-            GameObject ovrAvatar = PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath("Assets/Oculus/Avatar/Content/Prefabs/LocalAvatar.prefab", typeof(GameObject))) as GameObject;
+            GameObject ovrAvatar = null;
+            GameObject ovrAvatarToInstantiate = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/Oculus/Avatar/Content/Prefabs/LocalAvatar.prefab", typeof(GameObject));
+            if (ovrAvatarToInstantiate != null)
+            {
+                ovrAvatar = Instantiate(ovrAvatarToInstantiate) as GameObject;
+            }
+
             if (ovrAvatar == null)
             {
                 //legacy location
-                ovrAvatar = PrefabUtility.InstantiatePrefab((GameObject)AssetDatabase.LoadAssetAtPath("Assets/OvrAvatar/Content/Prefabs/LocalAvatar.prefab", typeof(GameObject))) as GameObject;
+                ovrAvatarToInstantiate = (GameObject)AssetDatabase.LoadAssetAtPath("Assets/OvrAvatar/Content/Prefabs/LocalAvatar.prefab", typeof(GameObject));
+                if (ovrAvatarToInstantiate != null)
+                {
+                    ovrAvatar = Instantiate(ovrAvatarToInstantiate) as GameObject;
+                }
             }
             if (ovrAvatar != null)
             {
+                OvrAvatar avatarScript = ovrAvatar.GetComponent<OvrAvatar>();
+                avatarScript.StartWithControllers = true;
+                ovrAvatar.name = ovrAvatar.name.Replace("(Clone)", "");
                 ovrAvatar.transform.SetParent(oculusSDK.transform);
-                ovrAvatar.SetActive(false);
+                ovrAvatar.SetActive(true);
                 oculusSetup.modelAliasLeftController = GameObject.Find(oculusPath + "/LocalAvatar/controller_left");
                 oculusSetup.modelAliasRightController = GameObject.Find(oculusPath + "/LocalAvatar/controller_right");
                 GameObject.Find(oculusPath + "/LocalAvatar/hand_left").SetActive(false);
