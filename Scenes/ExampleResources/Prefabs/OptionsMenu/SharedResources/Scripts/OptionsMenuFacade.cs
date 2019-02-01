5cfd237171805bc1769ae8a1a5e233ea532bd3f8
Harvey Ball <me@harveyball.com>
2019-02-01 20:01:20 +0000

    feat(examples): add barnyard example scene
    The new barnyard example scene showcases a collection of features
    offered by the VRTK prefabs.

---
+++ Scenes/ExampleResources/Prefabs/OptionsMenu/SharedResources/Scripts/OptionsMenuFacade.cs
@@ -0,0 +1,99 @@
+﻿namespace VRTK.Examples.Prefabs.OptionsMenu
+{
+    using UnityEngine;
+    using Zinnia.Action;
+    using Zinnia.Data.Attribute;
+
+    /// <summary>
+    /// Displays a control station with options to change functionality at runtime.
+    /// </summary>
+    public class OptionsMenuFacade : MonoBehaviour
+    {
+        #region Options Menu Settings
+        [Header("Options Menu Settings"), Tooltip("The action that will toggle the visibility of the options control station."), SerializeField]
+        private BooleanAction _activationAction;
+        /// <summary>
+        /// The action that will toggle the visibility of the options control station.
+        /// </summary>
+        public BooleanAction ActivationAction
+        {
+            get { return _activationAction; }
+            set
+            {
+                _activationAction = value;
+                ConfigureToggleVisibility();
+            }
+        }
+
+        /// <summary>
+        /// The target location to set the control station to when it appears.
+        /// </summary>
+        [Tooltip("The target location to set the control station to when it appears.")]
+        public GameObject locationTarget;
+        /// <summary>
+        /// The direction the control station will be placed in relation to the target location when it appears.
+        /// </summary>
+        [Tooltip("The direction the control station will be placed in relation to the target location when it appears.")]
+        public GameObject locationDirection;
+        /// <summary>
+        /// The offset distance from the target location to place the control station when it appears.
+        /// </summary>
+        [Tooltip("The offset distance from the target location to place the control station when it appears.")]
+        public float locationOffset = 0.5f;
+        #endregion
+
+        #region Internal Settings
+        /// <summary>
+        /// The control station.
+        /// </summary>
+        [Header("Internal Settings"), Tooltip("The control station."), InternalSetting, SerializeField]
+        private GameObject controlStation = null;
+        /// <summary>
+        /// The internal linked action for controlling visibility activation.
+        /// </summary>
+        [Tooltip("The internal linked action for controlling visibility activation."), InternalSetting, SerializeField]
+        private BooleanAction linkedInternalAction = null;
+        #endregion
+
+        public virtual void SetLocation()
+        {
+            if (locationTarget == null || locationDirection == null)
+            {
+                return;
+            }
+
+            transform.position = new Vector3(locationDirection.transform.position.x, locationTarget.transform.position.y, locationDirection.transform.position.z);
+            controlStation.transform.localPosition = locationDirection.transform.forward * locationOffset;
+
+            Vector3.Scale(controlStation.transform.localPosition, Vector3.right + Vector3.forward);
+            Vector3 targetPosition = locationDirection.transform.position;
+            targetPosition.y = locationTarget.transform.position.y;
+            controlStation.transform.LookAt(targetPosition);
+            controlStation.transform.localEulerAngles = Vector3.up * (controlStation.transform.localEulerAngles.y + 180f);
+        }
+
+        protected virtual void OnEnable()
+        {
+            ConfigureToggleVisibility();
+        }
+
+        protected virtual void OnValidate()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            ConfigureToggleVisibility();
+        }
+
+        /// <summary>
+        /// Links the provided <see cref="ActivationAction"/> action to the internal <see cref="linkedInternalAction"/>.
+        /// </summary>
+        protected virtual void ConfigureToggleVisibility()
+        {
+            linkedInternalAction.ClearSources();
+            linkedInternalAction.AddSource(ActivationAction);
+        }
+    }
+}
---
+++ Scenes/ExampleResources/Scripts/Physics/CollisionIgnorer.cs
@@ -0,0 +1,78 @@
+﻿namespace VRTK.Examples.Physics
+{
+    using UnityEngine;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Ignores collisions between two collections of <see cref="GameObject"/>s.
+    /// </summary>
+    public class CollisionIgnorer : MonoBehaviour
+    {
+        /// <summary>
+        /// The <see cref="GameObject"/> collection to ignore collisions from.
+        /// </summary>
+        [Tooltip("The GameObjects to ignore collisions from.")]
+        public List<GameObject> ignoreCollisionsFrom = new List<GameObject>();
+        /// <summary>
+        /// The <see cref="GameObject"/> collection  to ignore collisions with.
+        /// </summary>
+        [Tooltip("The GameObjects to ignore collisions with.")]
+        public List<GameObject> ignoreCollisionsWith = new List<GameObject>();
+        /// <summary>
+        /// Whether to ignore collisions on component enable.
+        /// </summary>
+        [Tooltip("Whether to ignore collisions on component enable.")]
+        public bool ignoreCollisionsOnEnable = true;
+
+        /// <summary>
+        /// Ignores the collisions between the colliders found in the <see cref="ignoreCollisionsFrom"/> and the <see cref="ignoreCollisionsWith"/>.
+        /// </summary>
+        public virtual void IgnoreCollisions()
+        {
+            ToggleCollisions(true);
+        }
+
+        /// <summary>
+        /// Restores the collisions between the colliders found in the <see cref="ignoreCollisionsFrom"/> and the <see cref="ignoreCollisionsWith"/>.
+        /// </summary>
+        public virtual void RestoreCollisions()
+        {
+            ToggleCollisions(false);
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (ignoreCollisionsOnEnable)
+            {
+                IgnoreCollisions();
+            }
+        }
+
+        /// <summary>
+        /// Toggles the collision state between the colliders found in the <see cref="ignoreCollisionsFrom"/> and the <see cref="ignoreCollisionsWith"/>.
+        /// </summary>
+        /// <param name="ignore">Whether to ignore the collisions or restore the collisions.</param>
+        protected virtual void ToggleCollisions(bool ignore)
+        {
+            List<Collider> collidersFrom = new List<Collider>();
+            List<Collider> collidersWith = new List<Collider>();
+            foreach (GameObject ignoreCollisionFrom in ignoreCollisionsFrom)
+            {
+                collidersFrom.AddRange(ignoreCollisionFrom.GetComponentsInChildren<Collider>());
+            }
+            foreach (GameObject ignoreCollisionWith in ignoreCollisionsWith)
+            {
+                collidersWith.AddRange(ignoreCollisionWith.GetComponentsInChildren<Collider>());
+            }
+
+            foreach (Collider ignoreCollisionFromCollider in collidersFrom)
+            {
+                foreach (Collider ignoreCollisionWithCollider in collidersWith)
+                {
+                    Physics.IgnoreCollision(ignoreCollisionFromCollider, ignoreCollisionWithCollider, ignore);
+                }
+            }
+
+        }
+    }
+}
