637717d4df49f68a6681b2bc0baaba1b48947d18
Harvey Ball <me@harveyball.com>
2019-02-16 17:29:15 +0000

    feat(Prefabs): add tooltip prefab
    The Tooltip prefab allows a simple set up of a tooltip to display
    text and point to a specific location to provide useful context.

---
+++ Prefabs/Helpers/Tooltip/SharedResources/Scripts/TooltipFacade.cs
@@ -0,0 +1,180 @@
+﻿namespace VRTK.Prefabs.Helpers.Tooltip
+{
+    using UnityEngine;
+    using Zinnia.Data.Attribute;
+
+    /// <summary>
+    /// The public interface for the TooltipFacade prefab.
+    /// </summary>
+    public class TooltipFacade : MonoBehaviour
+    {
+        #region Text Settings
+        [Header("Text Settings"), Tooltip("The text to display in the tooltip."), SerializeField]
+        private string displayText = "Tooltip Text";
+        /// <summary>
+        /// The text to display in the tooltip.
+        /// </summary>
+        public string DisplayText
+        {
+            get
+            {
+                return displayText;
+            }
+            set
+            {
+                displayText = value;
+                internalSetup.SetDisplayText(displayText, TextSize, TextColor);
+            }
+        }
+
+        [Tooltip("The size of the text for the tooltip."), SerializeField]
+        private int textSize = 14;
+        /// <summary>
+        /// The size of the text for the tooltip.
+        /// </summary>
+        public int TextSize
+        {
+            get
+            {
+                return textSize;
+            }
+            set
+            {
+                textSize = value;
+                internalSetup.SetDisplayText(DisplayText, textSize, TextColor);
+            }
+        }
+
+        [Tooltip("The color of the text for the tooltip."), SerializeField]
+        private Color textColor = Color.black;
+        /// <summary>
+        /// The color of the text for the tooltip.
+        /// </summary>
+        public Color TextColor
+        {
+            get
+            {
+                return textColor;
+            }
+            set
+            {
+                textColor = value;
+                internalSetup.SetDisplayText(DisplayText, TextSize, textColor);
+            }
+        }
+        #endregion
+
+        #region Container Settings
+        [Header("Container Settings"), Tooltip("The size of the background for the tooltip container."), SerializeField]
+        private Vector2 containerSize = new Vector2(100f, 30f);
+        /// <summary>
+        /// The size of the background for the tooltip container.
+        /// </summary>
+        public Vector2 ContainerSize
+        {
+            get
+            {
+                return containerSize;
+            }
+            set
+            {
+                containerSize = value;
+                internalSetup.SetContainer(containerSize, ContainerColor);
+            }
+        }
+
+        [Tooltip("The color of the background for the tooltip container."), SerializeField]
+        private Color containerColor = Color.white;
+        /// <summary>
+        /// The color of the background for the tooltip container.
+        /// </summary>
+        public Color ContainerColor
+        {
+            get
+            {
+                return containerColor;
+            }
+            set
+            {
+                containerColor = value;
+                internalSetup.SetContainer(ContainerSize, containerColor);
+            }
+        }
+
+        [Tooltip("The object that the tooltip will face towards."), SerializeField]
+        private GameObject facingTarget;
+        /// <summary>
+        /// The object that the tooltip will face towards.
+        /// </summary>
+        public GameObject FacingTarget
+        {
+            get
+            {
+                return facingTarget;
+            }
+            set
+            {
+                facingTarget = value;
+            }
+        }
+        #endregion
+
+        #region Line Settings
+        [Header("Line Settings"), Tooltip("The target to draw the tooltip line to."), SerializeField]
+        private GameObject lineTarget;
+        /// <summary>
+        /// The target to draw the tooltip line to.
+        /// </summary>
+        public GameObject LineTarget
+        {
+            get
+            {
+                return lineTarget;
+            }
+            set
+            {
+                lineTarget = value;
+                internalSetup.SetLine(lineTarget, LineWidth);
+            }
+        }
+
+        [Tooltip("The width of the line for the tooltip."), SerializeField]
+        private float lineWidth = 0.001f;
+        /// <summary>
+        /// The width of the line for the tooltip.
+        /// </summary>
+        public float LineWidth
+        {
+            get
+            {
+                return lineWidth;
+            }
+            set
+            {
+                lineWidth = value;
+                internalSetup.SetLine(LineTarget, lineWidth);
+            }
+        }
+        #endregion
+
+        #region Internal Settings
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        [Header("Internal Settings"), Tooltip("The linked Internal Setup."), InternalSetting, SerializeField]
+        protected TooltipInternalSetup internalSetup;
+        #endregion
+
+        protected virtual void OnValidate()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            internalSetup.SetDisplayText(DisplayText, TextSize, TextColor);
+            internalSetup.SetContainer(ContainerSize, ContainerColor);
+            internalSetup.SetLine(LineTarget, LineWidth);
+        }
+    }
+}
---
+++ Prefabs/SharedResources/Shaders/UI_Overlay.shader
@@ -0,0 +1,95 @@
+﻿// UNITY_SHADER_NO_UPGRADE
+Shader "VRTK/UI/Overlay"
+{
+	Properties
+	{
+		[PerRendererData] _MainTex("Font Texture", 2D) = "white" {}
+
+		_Color("Tint", Color) = (1,1,1,1)
+		_StencilComp("Stencil Comparison", Float) = 8
+		_Stencil("Stencil ID", Float) = 0
+		_StencilOp("Stencil Operation", Float) = 0
+		_StencilWriteMask("Stencil Write Mask", Float) = 255
+		_StencilReadMask("Stencil Read Mask", Float) = 255
+		_ColorMask("Color Mask", Float) = 15
+	}
+
+	SubShader
+	{
+		LOD 100
+
+		Tags
+		{
+			"Queue" = "Transparent"
+			"IgnoreProjector" = "True"
+			"RenderType" = "Transparent"
+			"PreviewType" = "Plane"
+			"CanUseSpriteAtlas" = "True"
+		}
+
+		Stencil
+		{
+			Ref[_Stencil]
+			Comp[_StencilComp]
+			Pass[_StencilOp]
+			ReadMask[_StencilReadMask]
+			WriteMask[_StencilWriteMask]
+		}
+
+		Cull Back
+		Lighting Off
+		ZWrite Off
+		ZTest[unity_GUIZTestMode]
+		Offset -1, -1
+		Blend SrcAlpha OneMinusSrcAlpha
+		ColorMask[_ColorMask]
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			#include "UnityUI.cginc"
+
+			struct appdata_t
+			{
+				float4 vertex : POSITION;
+				float2 texcoord : TEXCOORD0;
+				float4 color : COLOR;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				half2 texcoord : TEXCOORD0;
+				fixed4 color : COLOR;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			fixed4 _Color;
+			fixed4 _TextureSampleAdd;
+
+			v2f vert(appdata_t v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.texcoord = TRANSFORM_TEX(v.texcoord, _MainTex);
+				o.color = v.color * _Color;
+#ifdef UNITY_HALF_TEXEL_OFFSET
+				o.vertex.xy += (_ScreenParams.zw - 1.0)*float2(-1,1);
+#endif
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				fixed4 col = (tex2D(_MainTex, i.texcoord) + _TextureSampleAdd) * i.color;
+				clip(col.a - 0.01);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
