fba501fc62b7cc61d3e0d37548aaa8715ebe2017
Harvey Ball <me@harveyball.com>
2019-02-18 09:23:26 +0000

    feat(examples): add warehouse door with interactable buttons to control
    A warehouse door has been added that can be slid up and down based on
    pressing physics buttons. This showcases the TrackedCollider prefab as
    well as the controller colliders are only enabled when in proximity of
    the buttons and will use rigidbody physics to track the position of
    the controller. This means that the controller colliders cannot pass
    through the buttons and therefore won't prevent the unity physics from
    glitching.

---
+++ Samples/Farm/Prefabs/WarehouseDoor/SharedResources/Scripts/WarehouseDoor.cs
@@ -0,0 +1,37 @@
+﻿namespace VRTK.Examples.Prefabs.WarehouseDoor
+{
+    using UnityEngine;
+    using Zinnia.Data.Type;
+    using Zinnia.Extension;
+
+    /// <summary>
+    /// Controls the motion of the warehouse door.
+    /// </summary>
+    public class WarehouseDoor : MonoBehaviour
+    {
+        /// <summary>
+        /// The door to control.
+        /// </summary>
+        [Tooltip("The door to control.")]
+        public GameObject door;
+        /// <summary>
+        /// The minimum and maximum height of the door.
+        /// </summary>
+        [Tooltip("The minimum and maximum height of the door.")]
+        public FloatRange heightLimits = FloatRange.Zero;
+
+        /// <summary>
+        /// The step to increase or decrease the height of the door by.
+        /// </summary>
+        public float HeightStep { get; set; }
+
+        protected virtual void Update()
+        {
+            if (!HeightStep.ApproxEquals(0f))
+            {
+                float newHeight = Mathf.Clamp(door.transform.localScale.y + HeightStep, heightLimits.minimum, heightLimits.maximum);
+                door.transform.localScale = new Vector3(1f, newHeight, 1f);
+            }
+        }
+    }
+}
