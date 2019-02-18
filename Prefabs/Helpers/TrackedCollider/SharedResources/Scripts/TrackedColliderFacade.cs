b1cbc8a19139b923798c0415f3fed4d42a81c49e
Harvey Ball <me@harveyball.com>
2019-02-18 09:19:44 +0000

    feat(Helpers): colliders that track source location within physics
    The TrackedCollider prefab allows a collider to follow a source
    GameObject in the scene using rigidbody physics so it adheres to the
    physics rules within the world.
    
    This is useful for setting up controller colliders that cannot directly
    follow the controller 1:1 if the controller goes through geometry.
    
    This allows a better interaction with joint objects.

---
+++ Prefabs/Helpers/TrackedCollider/SharedResources/Scripts/TrackedColliderFacade.cs
@@ -0,0 +1,57 @@
+﻿namespace VRTK.Prefabs.Helpers.TrackedCollider
+{
+    using UnityEngine;
+    using Zinnia.Data.Attribute;
+
+    /// <summary>
+    /// The public interface for the TrackedCollider prefab.
+    /// </summary>
+    public class TrackedColliderFacade : MonoBehaviour
+    {
+        #region Tracking Settings
+        [Header("Tracking Settings"), Tooltip("The source to track."), SerializeField]
+        private GameObject source;
+        /// <summary>
+        /// The source to track.
+        /// </summary>
+        public GameObject Source
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                source = value;
+                internalSetup.SetSource(source);
+            }
+        }
+        #endregion
+
+        #region Internal Settings
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        [Header("Internal Settings"), Tooltip("The linked Internal Setup."), InternalSetting, SerializeField]
+        protected TrackedColliderInternalSetup internalSetup;
+        #endregion
+
+        /// <summary>
+        /// Snaps the tracked collider directly to the source current position.
+        /// </summary>
+        public virtual void SnapToSource()
+        {
+            internalSetup.SnapToSource();
+        }
+
+        protected virtual void OnValidate()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            internalSetup.SetSource(Source);
+        }
+    }
+}
---
+++ Prefabs/Helpers/TrackedCollider/SharedResources/Scripts/TrackedColliderInternalSetup.cs
@@ -0,0 +1,57 @@
+﻿namespace VRTK.Prefabs.Helpers.TrackedCollider
+{
+    using UnityEngine;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Data.Operation;
+    using Zinnia.Tracking.Follow;
+
+    /// <summary>
+    /// Sets up the TrackedCollider prefab based on the provided settings and implements the logic to follow the relevant source.
+    /// </summary>
+    public class TrackedColliderInternalSetup : MonoBehaviour
+    {
+        #region Facade Settings
+        /// <summary>
+        /// The public interface facade.
+        /// </summary>
+        [Header("Facade Settings"), Tooltip("The public interface facade."), InternalSetting, SerializeField]
+        protected TrackedColliderFacade facade;
+        #endregion
+
+        #region Reference Settings
+        /// <summary>
+        /// The <see cref="ObjectFollower"/> that performs the source follow.
+        /// </summary>
+        [Header("Reference Settings"), Tooltip("The ObjectFollower that performs the source follow."), InternalSetting, SerializeField]
+        protected ObjectFollower objectFollower;
+        /// <summary>
+        /// The <see cref="TransformPositionExtractor"/> that extracts the source position.
+        /// </summary>
+        [Tooltip("The TransformPositionExtractor that extracts the source position."), InternalSetting, SerializeField]
+        protected TransformPositionExtractor positionExtractor;
+        #endregion
+
+        /// <summary>
+        /// Sets the source on the relevant references.
+        /// </summary>
+        /// <param name="source">The source to set.</param>
+        public virtual void SetSource(GameObject source)
+        {
+            objectFollower.AddSource(source);
+            positionExtractor.source = source;
+        }
+
+        /// <summary>
+        /// Snaps the tracked collider directly to the source current position.
+        /// </summary>
+        public virtual void SnapToSource()
+        {
+            positionExtractor.DoExtract();
+        }
+
+        protected virtual void OnEnable()
+        {
+            SetSource(facade.Source);
+        }
+    }
+}
