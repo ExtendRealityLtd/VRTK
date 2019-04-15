47899676478b451938a8c366074d3bac453b5296
Harvey Ball <me@harveyball.com>
2019-04-15 10:22:20 +0100

    feat(Interactions): extract common references from Interactables
    The InteractableFacade can now have the ConsumerContainer and the
    ConsumerRigidbody properties extracted and emitted via a
    GameObjectExtractor implementation to make it easy to retrieve the
    appropriate elements from the InteractableFacade.

---
+++ Prefabs/Interactions/Interactables/SharedResources/Scripts/InteractableConsumerRigidbodyExtractor.cs
@@ -0,0 +1,37 @@
+﻿namespace VRTK.Prefabs.Interactions.Interactables
+{
+    using UnityEngine;
+    using Zinnia.Data.Operation.Extraction;
+
+    /// <summary>
+    /// Extracts the <see cref="GameObject"/> of the <see cref="InteractableFacade.ConsumerRigidbody"/>.
+    /// </summary>
+    public class InteractableConsumerRigidbodyExtractor : GameObjectExtractor
+    {
+        /// <summary>
+        /// Extracts the <see cref="GameObject"/> of the <see cref="InteractableFacade.ConsumerRigidbody"/>.
+        /// </summary>
+        /// <param name="interactable">The Interactable to extract from.</param>
+        /// <returns>The related <see cref="GameObject"/>.</returns>
+        public virtual GameObject Extract(InteractableFacade interactable)
+        {
+            if (interactable == null)
+            {
+                Result = null;
+                return null;
+            }
+
+            Result = interactable.ConsumerRigidbody != null ? interactable.ConsumerRigidbody.gameObject : null;
+            return base.Extract();
+        }
+
+        /// <summary>
+        /// Extracts the <see cref="GameObject"/> of the <see cref="InteractableFacade.ConsumerRigidbody"/>.
+        /// </summary>
+        /// <param name="interactable">The Interactable to extract from.</param>
+        public virtual void DoExtract(InteractableFacade interactable)
+        {
+            Extract(interactable);
+        }
+    }
+}
