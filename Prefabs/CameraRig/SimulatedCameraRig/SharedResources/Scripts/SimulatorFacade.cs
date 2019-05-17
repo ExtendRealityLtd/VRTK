9736e2378379fcefa3a903c6498a7f667b4d8410
Harvey Ball <me@harveyball.com>
2019-05-17 07:01:10 +0100

    fix(CameraRig): use appropriate targets for controlling simulator
    The SimulatedCameraRig didn't correctly represent how an actual
    VR CameraRig is set up. The need to inject the TrackedAlias to
    move the simulator rig around is incorrect and in fact the simulator
    should not be allowing the movement of the CameraRig (play area)
    as this is not something that happens with actual VR rigs. Instead
    a player can walk around their play area but this is just moving their
    headset and controller tracked avatars within the play area and then
    the play area is moved via other mechanisms.
    
    This fix removes the need to inject a TrackedAlias into the Simulator
    and now the movement only controls the position of the headset within
    the play area.
    
    The speed that the simulated avatar objects can now also be set on the
    SimulatorFacade.

--- Prefabs/CameraRig/SimulatedCameraRig/SharedResources/Scripts/SimulatorFacade.cs
+++ Prefabs/CameraRig/SimulatedCameraRig/SharedResources/Scripts/SimulatorFacade.cs
@@ -1,13 +1,10 @@
 ﻿namespace VRTK.Prefabs.CameraRig.SimulatedCameraRig
 {
     using UnityEngine;
-    using UnityEngine.XR;
     using Malimbe.MemberChangeMethod;
     using Malimbe.XmlDocumentationAttribute;
     using Malimbe.PropertySerializationAttribute;
     using Zinnia.Data.Attribute;
-    using Zinnia.Data.Operation.Mutation;
-    using VRTK.Prefabs.CameraRig.TrackedAlias;
 
     /// <summary>
     /// The public interface into the SimulatedCameraRig Prefab.
@@ -16,16 +13,25 @@
     {
         #region Simulator Settings
         /// <summary>
-        /// The optional Tracked Alias prefab, must be provided if one is used in the scene.
+        /// The speed at which to move the player around via the default movement keys.
         /// </summary>
         [Serialized]
         [field: Header("Simulator Settings"), DocumentedByXml]
-        public TrackedAliasFacade TrackedAlias { get; set; }
+        public float PlayerSpeed { get; set; } = 0.05f;
         /// <summary>
-        /// Determines whether to disable the XRSettings.
+        /// The speed at which to move the controllers around via the default movement keys.
         /// </summary>
         [Serialized]
         [field: DocumentedByXml]
+        public float ControllerSpeed { get; set; } = 0.025f;
+        #endregion
+
+        #region System Settings
+        /// <summary>
+        /// Determines whether to disable <see cref="XRSettings.enabled"/>.
+        /// </summary>
+        [Serialized]
+        [field: Header("System Settings"), DocumentedByXml]
         public bool DisableXRSettings { get; set; } = true;
         /// <summary>
         /// The frame rate to simulate with fixedDeltaTime.
@@ -37,89 +43,29 @@
 
         #region Reference Settings
         /// <summary>
-        /// The linked TransformPositionMutator.
+        /// The linked Internal Setup.
         /// </summary>
         [Serialized]
         [field: Header("Reference Settings"), DocumentedByXml, Restricted]
-        public TransformPositionMutator PlayAreaPosition { get; protected set; }
-        /// <summary>
-        /// The linked TransformPropertyResetter.
-        /// </summary>
-        [Serialized]
-        [field: DocumentedByXml, Restricted]
-        public TransformPropertyResetter PlayAreaResetter { get; protected set; }
+        public SimulatorConfigurator Configuration { get; protected set; }
         #endregion
 
         /// <summary>
-        /// The original configuration of XRSettings.
+        /// Called after <see cref="PlayerSpeed"/> has been changed.
         /// </summary>
-        protected bool originalXRSettings;
-        /// <summary>
-        /// The original configuration of FixedDeltaTime.
-        /// </summary>
-        protected float originalFixedDeltaTime;
-
-        protected virtual void OnEnable()
+        [CalledAfterChangeOf(nameof(PlayerSpeed))]
+        protected virtual void OnAfterPlayerSpeedChange()
         {
-            originalXRSettings = XRSettings.enabled;
-            originalFixedDeltaTime = Time.fixedDeltaTime;
-            ConfigureTrackedAlias();
-            ConfigureXRSettings(DisableXRSettings);
-        }
-
-        protected virtual void OnDisable()
-        {
-            ConfigureXRSettings(false);
-            Time.fixedDeltaTime = originalFixedDeltaTime;
+            Configuration.ConfigureControlSpeed();
         }
 
         /// <summary>
-        /// Configures the provided <see cref="TrackedAlias"/> onto the simulator CameraRig.
+        /// Called after <see cref="ControllerSpeed"/> has been changed.
         /// </summary>
-        protected virtual void ConfigureTrackedAlias()
+        [CalledAfterChangeOf(nameof(ControllerSpeed))]
+        protected virtual void OnAfterControllerSpeedChange()
         {
-            if (TrackedAlias == null)
-            {
-                return;
-            }
-
-            GameObject playAreaObject = TrackedAlias.PlayAreaAlias.gameObject;
-            PlayAreaPosition.Target = PlayAreaPosition != null ? playAreaObject : null;
-            PlayAreaResetter.Source = PlayAreaResetter != null ? playAreaObject : null;
-        }
-
-        /// <summary>
-        /// Configures the XRSettings.
-        /// </summary>
-        /// <param name="state">The new value for the setting.</param>
-        protected virtual void ConfigureXRSettings(bool state)
-        {
-            if (state)
-            {
-                originalXRSettings = XRSettings.enabled;
-                XRSettings.enabled = false;
-            }
-            else
-            {
-                XRSettings.enabled = originalXRSettings;
-            }
-        }
-
-        /// <summary>
-        /// Configures the simulated frame rate.
-        /// </summary>
-        protected virtual void ConfigureSimulatedFrameRate()
-        {
-            Time.fixedDeltaTime = Time.timeScale / SimulatedFrameRate;
-        }
-
-        /// <summary>
-        /// Called after <see cref="TrackedAlias"/> has been changed.
-        /// </summary>
-        [CalledAfterChangeOf(nameof(TrackedAlias))]
-        protected virtual void OnAfterTrackedAliasChange()
-        {
-            ConfigureTrackedAlias();
+            Configuration.ConfigureControlSpeed();
         }
 
         /// <summary>
@@ -128,7 +74,7 @@
         [CalledAfterChangeOf(nameof(DisableXRSettings))]
         protected virtual void OnAfterDisableXRSettingsChange()
         {
-            ConfigureXRSettings(DisableXRSettings);
+            Configuration.ConfigureXRSettings(DisableXRSettings);
         }
 
         /// <summary>
@@ -137,7 +83,7 @@
         [CalledAfterChangeOf(nameof(SimulatedFrameRate))]
         protected virtual void OnAfterSimulatedFrameRateChange()
         {
-            ConfigureSimulatedFrameRate();
+            Configuration.ConfigureSimulatedFrameRate();
         }
     }
 }
