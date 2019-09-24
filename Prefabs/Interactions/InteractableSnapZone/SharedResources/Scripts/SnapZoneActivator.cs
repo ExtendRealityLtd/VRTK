37bb00625dad3768fa0e50ebc6deda8a8ccf253a
Harvey Ball <me@harveyball.com>
2019-09-24 14:38:07 +0100

    fix(Interactions): prevent object activating multiple snapzones
    There was nothing preventing an Interactable activating multiple
    SnapZones at the same time, but this raised an issue of when the object
    was released, then which zone should it snap to? It would snap to the
    first zone it collided with but it would also run the snap logic on the
    other SnapZone that it had activated.
    
    An object can only activate one SnapZone at a time, even though
    multiple objects can enter a SnapZone and all be potentially snappable
    to that zone. However, the first object entering is the only object
    that actually activates the zone.
    
    This has been accomplished by utilizing the new collision tracking on
    the Interactable and determining whether the interactable touching
    the current SnapZone is also colliding with another SnapZone. If it is
    then an event is registered for when the interactable leaves the
    current activated SnapZone to try and then reactivate any other
    SnapZones that it also may be touching.

---
+++ Prefabs/Interactions/InteractableSnapZone/SharedResources/Scripts/SnapZoneActivator.cs
@@ -0,0 +1,32 @@
+﻿namespace VRTK.Prefabs.Interactions.InteractableSnapZone
+{
+    using UnityEngine;
+    using Malimbe.XmlDocumentationAttribute;
+    using Malimbe.PropertySerializationAttribute;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Tracking.Collision;
+
+    public class SnapZoneActivator : MonoBehaviour
+    {
+        #region Facade Settings
+        /// <summary>
+        /// The public interface facade.
+        /// </summary>
+        [Serialized]
+        [field: Header("Facade Settings"), DocumentedByXml, Restricted]
+        public SnapZoneFacade Facade { get; protected set; }
+        /// <summary>
+        /// The <see cref="CollisionTracker"/> used for tracking collisions on the zone.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Restricted]
+        public CollisionTracker CollisionTracker { get; protected set; }
+        /// <summary>
+        /// The <see cref="Collider"/> that is used to determine the collidable area for the zone.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public Collider Collider { get; set; }
+        #endregion
+    }
+}
