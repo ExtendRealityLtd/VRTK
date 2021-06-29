f4a198b88194a2acc8159399c7823307a3f99b83
extendreality <hello@extendreality.io>
2021-06-29 16:48:15 +0100

    feat(examples): add wheel on water tower to control water flow
    An angular transform drive has been added to the water wheel that
    controls the flow of water pumping out of the water tower.

---
+++ Assets/Samples/Farm/Scripts/Runtime/ParticleChanger.cs
@@ -0,0 +1,22 @@
+﻿namespace VRTK.Examples
+{
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+
+    public class ParticleChanger : MonoBehaviour
+    {
+        /// <summary>
+        /// The <see cref="ParticleSystem"/> to control.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public ParticleSystem Particles { get; set; }
+
+        public virtual void UpdateEmissionRate(float rate)
+        {
+            ParticleSystem.EmissionModule emissions = Particles.emission;
+            emissions.rateOverTime = rate;
+        }
+    }
+}
